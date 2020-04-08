b68c3f6dda805c0fa32e0e939d50f0e6e2e3cd0e
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2020-04-08 16:56:22 +0700

    Enhance Unit Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/DyeingPrintingAreaMovementRepositoryTest.cs
@@ -0,0 +1,92 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.DyeingPrintingAreaMovement;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Com.Danliris.Service.Packing.Inventory.Data.Models;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Infrastructure
+{
+    public class DyeingPrintingAreaMovementRepositoryTest
+    {
+        private const string ENTITY = "DyeingPrintintAreaMovement";
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetCurrentMethod()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            return string.Concat(sf.GetMethod().Name, "_", ENTITY);
+        }
+
+        private PackingInventoryDbContext GetDbContext(string testName)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PackingInventoryDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(testName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+
+            var dbContext = new PackingInventoryDbContext(optionsBuilder.Options);
+
+            return dbContext;
+        }
+
+        private DyeingPrintingAreaMovementRepository GetRepository(PackingInventoryDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            return new DyeingPrintingAreaMovementRepository(dbContext, serviceProvider);
+        }
+
+        private DyeingPrintingAreaMovementModel Model
+        {
+            get
+            {
+                return new DyeingPrintingAreaMovementModel("area", "no", DateTimeOffset.UtcNow, "shift", 1, "code", "no", 1, "type", "buyer", "inst", "1-2",
+                    1, "code", "name", 1, "code", "name", "1", 1, "code", "name", "color", "mtf", "awal", 1, "MTR", 1, "OK", "A", "area");
+            }
+            set { }
+        }
+
+        private Mock<IServiceProvider> GetServiceProvider()
+        {
+            Mock<IServiceProvider> sp = new Mock<IServiceProvider>();
+            sp.Setup(s => s.GetService(typeof(IIdentityProvider)))
+                .Returns(new IdentityProvider() { TimezoneOffset = 7, Token = "token", Username = "username" });
+
+            return sp;
+        }
+
+        [Fact]
+        public async Task Should_Success_Insert()
+        {
+            string testName = GetCurrentMethod();
+            var dbContext = GetDbContext(testName);
+
+            var repo = GetRepository(dbContext, GetServiceProvider().Object);
+
+            var result = await repo.InsertAsync(Model);
+            Assert.NotEqual(0, result);
+        }
+
+        //[Fact]
+        //public async Task Should_Success_Delete()
+        //{
+        //    string testName = GetCurrentMethod();
+        //    var dbContext = GetDbContext(testName);
+
+        //    var repo = GetRepository(dbContext, GetServiceProvider().Object);
+        //    await repo.InsertAsync(Model);
+
+        //    var result = await repo.DeleteAsync(Model.Id);
+
+        //    Assert.NotEqual(0, result);
+        //}
+    }
+}
