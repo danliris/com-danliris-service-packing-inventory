ded11572d0ef89de4fcbf8215928a2e1531bfb32
Muhammad Fakhrian Putra <45648586+mfp-fakhrian@users.noreply.github.com>
2020-04-15 17:13:05 +0700

    Add Migration AddPropertiesOnDyeingPrintingAreaMovementHistoryModel

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200415101058_AddPropertiesOnDyeingPrintingAreaMovementHistoryModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class AddPropertiesOnDyeingPrintingAreaMovementHistoryModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "ProductionOrderQuantity",
+                table: "DyeingPrintingAreaMovementHistories",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "QtyKg",
+                table: "DyeingPrintingAreaMovementHistories",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UOMUnit",
+                table: "DyeingPrintingAreaMovementHistories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductionOrderQuantity",
+                table: "DyeingPrintingAreaMovementHistories");
+
+            migrationBuilder.DropColumn(
+                name: "QtyKg",
+                table: "DyeingPrintingAreaMovementHistories");
+
+            migrationBuilder.DropColumn(
+                name: "UOMUnit",
+                table: "DyeingPrintingAreaMovementHistories");
+        }
+    }
+}
