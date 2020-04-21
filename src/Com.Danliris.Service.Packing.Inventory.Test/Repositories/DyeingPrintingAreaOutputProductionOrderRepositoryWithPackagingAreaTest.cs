44e91e58df2da4064289aecfb40ddfe7b51d99e6
ryan-moonlay <ryan.santosa@moonlay.com>
2020-04-21 15:17:45 +0700

    Enhance Unit test For Packaing Area Requirement

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/DyeingPrintingAreaOutputProductionOrderRepositoryWithPackagingAreaTest.cs
@@ -0,0 +1,47 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.DyeingPrintingAreaMovement;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.DyeingPrintingAreaMovement;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories
+{
+    public class DyeingPrintingAreaOutputProductionOrderRepositoryWithPackagingAreaTest
+        : BaseRepositoryTest<PackingInventoryDbContext, DyeingPrintingAreaOutputProductionOrderRepository, DyeingPrintingAreaOutputProductionOrderModel, DyeingPrintingAreaOutputProductionOrderWithPackagingAreaDataUtil>
+    {
+        private const string ENTITY = "DyeingPrintingAreaOutputProductionOrder";
+        public DyeingPrintingAreaOutputProductionOrderRepositoryWithPackagingAreaTest() : base(ENTITY)
+        {
+        }
+
+        [Fact]
+        public async Task Should_Success_GetDbSet()
+        {
+            string testName = GetCurrentMethod();
+            var dbContext = DbContext(testName);
+
+            var repo = new DyeingPrintingAreaOutputProductionOrderRepository(dbContext, GetServiceProviderMock(dbContext).Object);
+            var data = await DataUtil(repo, dbContext).GetTestData();
+            var result = repo.GetDbSet();
+
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public async Task Should_Success_ReadAllIgnoreQueryFilter()
+        {
+            string testName = GetCurrentMethod();
+            var dbContext = DbContext(testName);
+
+            var repo = new DyeingPrintingAreaOutputProductionOrderRepository(dbContext, GetServiceProviderMock(dbContext).Object);
+            var data = await DataUtil(repo, dbContext).GetTestData();
+            var result = repo.ReadAllIgnoreQueryFilter();
+
+            Assert.NotEmpty(result);
+        }
+    }
+}
