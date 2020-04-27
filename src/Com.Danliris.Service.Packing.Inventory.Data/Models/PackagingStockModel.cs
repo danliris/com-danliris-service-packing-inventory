c65660be5f7c6bb3e3f20f806e00356f7ca28330
ryan-moonlay <ryan.santosa@moonlay.com>
2020-04-27 11:39:31 +0700

    enhance GetSetter PackagingStock

--- src/Com.Danliris.Service.Packing.Inventory.Data/Models/PackagingStockModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Data/Models/PackagingStockModel.cs
@@ -24,14 +24,67 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models
             HasNextArea = hasNextArea;
         }
 
-        public int DyeingPrintingProductionOrderId { get; set; }
-        public string ProductionOrderNo { get; set; }
-        public string PackagingType { get; set; }
-        public string PackagingUnit { get; set; }
-        public decimal PackagingQty { get; set; }
-        public string UomUnit { get; set; }
-        public decimal Length { get; set; }
-        public bool HasNextArea { get; set; }
+        public int DyeingPrintingProductionOrderId { get; private set; }
+        public string ProductionOrderNo { get; private set; }
+        public string PackagingType { get; private set; }
+        public string PackagingUnit { get; private set; }
+        public decimal PackagingQty { get; private set; }
+        public string UomUnit { get; private set; }
+        public decimal Length { get; private set; }
+        public bool HasNextArea { get; private set; }
 
+        public void SetPackagingType(string newPackagingType, string user, string agent)
+        {
+            if(newPackagingType != PackagingType)
+            {
+                PackagingType = newPackagingType;
+                this.FlagForUpdate(user, agent);
+            }
+        }
+        public void SetPackagingUnit(string newPackagingUnit, string user, string agent)
+        {
+            if(newPackagingUnit!= PackagingUnit)
+            {
+                PackagingUnit = newPackagingUnit;
+                this.FlagForUpdate(user, agent);
+            }
+        }
+
+        public void SetPackagingQty(decimal newPackagingQty,string user, string agent)
+        {
+            if(newPackagingQty != PackagingQty)
+            {
+                PackagingQty = newPackagingQty;
+                this.FlagForUpdate(user, agent);
+
+            }
+        }
+
+        public void SetUomUnit(string newUomUnit, string user, string agent)
+        {
+            if(newUomUnit != UomUnit)
+            {
+                UomUnit = newUomUnit;
+                this.FlagForUpdate(user, agent);
+            }
+        }
+
+        public void SetLength(decimal newLength, string user, string agent)
+        {
+            if(newLength != Length)
+            {
+                Length = newLength;
+                this.FlagForUpdate(user, agent);
+            }
+        }
+
+        public void SetHasNextArea(bool hasNextArea, string user, string agent)
+        {
+            if(hasNextArea != HasNextArea)
+            {
+                HasNextArea = hasNextArea;
+                this.FlagForUpdate(user, agent);
+            }
+        }
     }
 }
--- src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/PackagingStockRepositoryTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/PackagingStockRepositoryTest.cs
@@ -5,14 +5,40 @@ using Com.Danliris.Service.Packing.Inventory.Test.DataUtils;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+
 
 namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories
 {
     public class PackagingStockRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, PackagingStockRepository, PackagingStockModel, PackagingStockDataUtil>
     {
         private const string Entity = "PackagingStock";
+
         public PackagingStockRepositoryTest() : base(Entity)
         {
+        }
+
+        [Fact]
+        public async void Should_Success_SetVoid()
+        {
+            string testName = GetCurrentMethod();
+            var dbContext = DbContext(testName);
+
+            var repo = new PackagingStockRepository(dbContext, GetServiceProviderMock(dbContext).Object);
+            var data = await DataUtil(repo, dbContext).GetTestData();
+            data.SetPackagingQty(10, "unitTest", "xUnit");
+            data.SetPackagingType("WHITE", "unitTest", "xUnit");
+            data.SetPackagingUnit("ROLLS", "unitTest", "xUnit");
+            data.SetLength(10, "unitTest", "xUnit");
+            data.SetUomUnit("METER", "unitTest", "xUnit");
+            data.SetHasNextArea(false, "unitTest", "xUnit");
+
+            Assert.Equal(10, data.PackagingQty);
+            Assert.Equal("WHITE", data.PackagingType);
+            Assert.Equal("ROLLS", data.PackagingUnit);
+            Assert.Equal(10, data.Length);
+            Assert.Equal("METER", data.UomUnit);
+            Assert.False(data.HasNextArea);
         }
     }
 }
