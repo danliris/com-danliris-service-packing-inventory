21360af7e6d66a14124a8aabc236645a4d3734d5
Muhammad Fakhrian Putra <45648586+mfp-fakhrian@users.noreply.github.com>
2020-05-04 22:39:02 +0700

    Add Unit Test Per Properties for IndexViewModel Aval Output

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaOutput/Aval/IndexViewModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaOutput/Aval/IndexViewModel.cs
@@ -5,21 +5,14 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Dyei
 {
     public class IndexViewModel
     {
-        public IndexViewModel()
-        {
-            AvalProductionOrders = new HashSet<OutputAvalItemViewModel>();
-        }
-
         public int Id { get; set; }
-        public string Area { get; set; }
         public DateTimeOffset Date { get; set; }
         public string BonNo { get; set; }
         public string Shift { get; set; }
-        public string AvalType { get; set; }
         public string CartNo { get; set; }
+        public string AvalType { get; set; }
         public string UomUnit { get; set; }
-        public string Qty { get; set; }
-        public string QtyKg { get; set; }
-        public ICollection<OutputAvalItemViewModel> AvalProductionOrders { get; set; }
+        public double Qty { get; set; }
+        public double QtyKg { get; set; }
     }
 }
