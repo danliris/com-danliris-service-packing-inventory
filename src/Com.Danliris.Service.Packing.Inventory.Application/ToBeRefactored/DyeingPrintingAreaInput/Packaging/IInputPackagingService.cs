4c5d13c52f4ccc57f04c42cd61c09a15a3bfc975
ryan-moonlay <ryan.santosa@moonlay.com>
2020-05-05 14:41:56 +0700

    Cleansing Packaging Input Area
    cleansing api with comment that method not used, it cause unit test coverage

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaInput/Packaging/IInputPackagingService.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaInput/Packaging/IInputPackagingService.cs
@@ -13,6 +13,6 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Dyei
         ListResult<IndexViewModel> Read(int page, int size, string filter, string order, string keyword);
         ListResult<InputPackagingProductionOrdersViewModel> ReadProductionOrders(int page, int size, string filter, string order, string keyword);
         ListResult<IndexViewModel> ReadBonOutToPack(int page, int size, string filter, string order, string keyword);
-        ListResult<InputPackagingProductionOrdersViewModel> ReadInProducionOrders(int page, int size, string filter, string order, string keyword);
+        //ListResult<InputPackagingProductionOrdersViewModel> ReadInProducionOrders(int page, int size, string filter, string order, string keyword);
     }
 }
