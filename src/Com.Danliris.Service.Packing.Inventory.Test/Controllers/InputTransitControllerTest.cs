beb68f15ad044880b9e9c503911020474838e442
skydivine <17161989+skydivine@users.noreply.github.com>
2020-05-12 11:55:00 +0700

    Enhance Unit Test

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/InputTransitControllerTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/InputTransitControllerTest.cs
@@ -272,5 +272,43 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers
 
             Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
         }
+
+        [Fact]
+        public void Should_Success_GetProductionOrders()
+        {
+            //v
+            var serviceMock = new Mock<IInputTransitService>();
+            serviceMock.Setup(s => s.GetOutputPreTransitProductionOrders())
+                .Returns(new List<OutputPreTransitProductionOrderViewModel>() { });
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider);
+            //controller.ModelState.IsValid == false;
+            var response = controller.GetProductionOrders();
+
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void Should_Exception_GetProductionOrders()
+        {
+            var dataUtil = ViewModel;
+            //v
+            var serviceMock = new Mock<IInputTransitService>();
+            serviceMock.Setup(s => s.GetOutputPreTransitProductionOrders()).Throws(new Exception());
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider);
+            //controller.ModelState.IsValid == false;
+            var response = controller.GetProductionOrders();
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
     }
 }
