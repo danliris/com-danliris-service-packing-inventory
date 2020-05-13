bb625d1bd3766d01caa13d722212384d8213ebd0
Muhammad Fakhrian Putra <45648586+mfp-fakhrian@users.noreply.github.com>
2020-05-13 19:10:23 +0700

    Add Date Validation on Goods Warehouse Input

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaInput/Warehouses/InputWarehousesValidator.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingAreaInput/Warehouses/InputWarehousesValidator.cs
@@ -11,7 +11,8 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Dyei
         public InputWarehousesValidator()
         {
             RuleFor(data => data.Area).NotNull().WithMessage("Harus Memiliki Area!");
-            RuleFor(data => data.Date).Must(s => s != default).WithMessage("Tanggal Harus Diisi!");
+            RuleFor(data => data.Date).Must(s => s != default).WithMessage("Tanggal Harus Diisi!")
+                .Must(s => s.Date >= DateTime.Now.Date).WithMessage("Tanggal Tidak Boleh Kurang Dari Hari Ini");
             RuleFor(data => data.Shift).NotNull().WithMessage("Shift Harus Diisi!");
             //RuleFor(data => data.BonNo).NotNull().WithMessage("No. Bon Harus Diisi!");
         }
