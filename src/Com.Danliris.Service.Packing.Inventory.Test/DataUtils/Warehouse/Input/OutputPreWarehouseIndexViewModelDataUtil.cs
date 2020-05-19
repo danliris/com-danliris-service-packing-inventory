8762561e86d2ee5be811a7513ee33261f7508450
Muhammad Fakhrian Putra <45648586+mfp-fakhrian@users.noreply.github.com>
2020-05-19 22:16:49 +0700

    Add Unit Test for OutputPreWarehouseIndexViewModel

--- src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/Warehouse/Input/OutputPreWarehouseIndexViewModelDataUtil.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/Warehouse/Input/OutputPreWarehouseIndexViewModelDataUtil.cs
@@ -24,29 +24,36 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
                         No = "F/2020/000",
                         OrderQuantity = 12
                     },
-                    ProductionOrderNo = "asd",
-                    CartNo = "5-11",
-                    PackingInstruction = "a",
+                    CartNo = "9",
+                    Buyer = "ANAS",
                     Construction = "a",
                     Unit = "a",
-                    Buyer = "a",
                     Color = "a",
                     Motif = "a",
                     UomUnit = "a",
-                    Balance = 10,
-                    HasNextAreaDocument = false,
-                    Grade = "a",
                     Remark = "a",
+                    Grade = "a",
                     Status = "a",
-                    //Material = "a",
-                    //MtrLength = 10,
-                    //YdsLength = 10,
-                    //Quantity = 10,
-                    PackagingType = "s",
-                    PackagingUnit = "a",
+                    Balance = 100,
+                    PackingInstruction = "a",
+                    PackagingType = "a",
                     PackagingQty = 10,
-                    QtyOrder = 10,
-                    OutputId = 1,
+                    PackagingUnit = "a",
+                    AvalALength = 10,
+                    AvalBLength = 10,
+                    AvalConnectionLength = 10,
+                    DeliveryOrderSalesId = 3,
+                    DeliveryOrderSalesNo = "a",
+                    AvalType = "a",
+                    AvalCartNo = "a",
+                    AvalQuantityKg = 5,
+                    Description = "a",
+                    DeliveryNote = "a",
+                    Area = "a",
+                    DestinationArea = "a",
+                    HasNextAreaDocument = false,
+                    DyeingPrintingAreaInputProductionOrderId = 4,
+                    OutputId = 10
                 };
             }
         }
@@ -76,7 +83,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         public void Should_ValidatorProductionOrderNo_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotNull(dataUtil.ProductionOrderNo);
+            Assert.NotNull(dataUtil.ProductionOrder.No);
         }
 
         [Fact]
@@ -101,10 +108,10 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         }
 
         [Fact]
-        public void Should_ValidatorPackingInstruction_Success()
+        public void Should_ValidatorBuyer_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotNull(dataUtil.PackingInstruction);
+            Assert.NotNull(dataUtil.Buyer);
         }
 
         [Fact]
@@ -119,13 +126,6 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
             Assert.NotNull(dataUtil.Unit);
-        }
-
-        [Fact]
-        public void Should_ValidatorBuyer_Success()
-        {
-            var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotNull(dataUtil.Buyer);
         }
 
         [Fact]
@@ -150,17 +150,10 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         }
 
         [Fact]
-        public void Should_ValidatorBalance_Success()
+        public void Should_ValidatorRemark_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotEqual(0, dataUtil.Balance);
-        }
-
-        [Fact]
-        public void Should_ValidatorHasNextAreaDocument_Success()
-        {
-            var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.False(dataUtil.HasNextAreaDocument);
+            Assert.NotNull(dataUtil.Remark);
         }
 
         [Fact]
@@ -171,59 +164,31 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         }
 
         [Fact]
-        public void Should_ValidatorRemark_Success()
-        {
-            var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotNull(dataUtil.Remark);
-        }
-
-        [Fact]
         public void Should_ValidatorStatus_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
             Assert.NotNull(dataUtil.Status);
         }
 
-        //[Fact]
-        //public void Should_ValidatorMaterial_Success()
-        //{
-        //    var dataUtil = OutputPreWarehouseIndexViewModel;
-        //    Assert.NotNull(dataUtil.Material);
-        //}
+        [Fact]
+        public void Should_ValidatorBalance_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.Balance);
+        }
 
-        //[Fact]
-        //public void Should_ValidatorMtrLength_Success()
-        //{
-        //    var dataUtil = OutputPreWarehouseIndexViewModel;
-        //    Assert.NotEqual(0, dataUtil.MtrLength);
-        //}
-
-        //[Fact]
-        //public void Should_ValidatorYdsLength_Success()
-        //{
-        //    var dataUtil = OutputPreWarehouseIndexViewModel;
-        //    Assert.NotEqual(0, dataUtil.YdsLength);
-        //}
-
-        //[Fact]
-        //public void Should_ValidatorQuantity_Success()
-        //{
-        //    var dataUtil = OutputPreWarehouseIndexViewModel;
-        //    Assert.NotEqual(0, dataUtil.Quantity);
-        //}
+        [Fact]
+        public void Should_ValidatorPackingInstruction_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.PackingInstruction);
+        }
 
         [Fact]
         public void Should_ValidatorPackagingType_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
             Assert.NotNull(dataUtil.PackagingType);
-        }
-
-        [Fact]
-        public void Should_ValidatorPackagingUnit_Success()
-        {
-            var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotNull(dataUtil.PackagingUnit);
         }
 
         [Fact]
@@ -234,10 +199,108 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Warehouse.Input
         }
 
         [Fact]
-        public void Should_ValidatorQtyOrder_Success()
+        public void Should_ValidatorPackagingUnit_Success()
         {
             var dataUtil = OutputPreWarehouseIndexViewModel;
-            Assert.NotEqual(0, dataUtil.QtyOrder);
+            Assert.NotNull(dataUtil.PackagingUnit);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalALength_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.AvalALength);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalBLength_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.AvalBLength);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalConnectionLength_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.AvalConnectionLength);
+        }
+
+        [Fact]
+        public void Should_ValidatorDeliveryOrderSalesId_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.DeliveryOrderSalesId);
+        }
+
+        [Fact]
+        public void Should_ValidatorDeliveryOrderSalesNo_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.DeliveryOrderSalesNo);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalType_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.AvalType);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalCartNo_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.AvalCartNo);
+        }
+
+        [Fact]
+        public void Should_ValidatorAvalQuantityKg_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.AvalQuantityKg);
+        }
+
+        [Fact]
+        public void Should_ValidatorDescription_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.Description);
+        }
+
+        [Fact]
+        public void Should_ValidatorDeliveryNote_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.DeliveryNote);
+        }
+
+        [Fact]
+        public void Should_ValidatorArea_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.Area);
+        }
+
+        [Fact]
+        public void Should_ValidatorDestinationArea_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotNull(dataUtil.DestinationArea);
+        }
+
+        [Fact]
+        public void Should_ValidatorHasNextAreaDocument_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.False(dataUtil.HasNextAreaDocument);
+        }
+
+        [Fact]
+        public void Should_ValidatorDyeingPrintingAreaInputProductionOrderId_Success()
+        {
+            var dataUtil = OutputPreWarehouseIndexViewModel;
+            Assert.NotEqual(0, dataUtil.DyeingPrintingAreaInputProductionOrderId);
         }
 
         [Fact]
