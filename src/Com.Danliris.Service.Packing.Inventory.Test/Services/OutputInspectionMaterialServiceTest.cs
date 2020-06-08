7b5576db946adf7e44ff55731104da084b86034c
skydivine <17161989+skydivine@users.noreply.github.com>
2020-06-08 14:05:12 +0700

    Update OutputInspectionMaterialServiceTest.cs

--- src/Com.Danliris.Service.Packing.Inventory.Test/Services/OutputInspectionMaterialServiceTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Services/OutputInspectionMaterialServiceTest.cs
@@ -529,6 +529,9 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Services
             repoMock.Setup(s => s.ReadByIdAsync(It.IsAny<int>()))
                 .ReturnsAsync(Model);
 
+            sppRepoMock.Setup(s => s.ReadByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new DyeingPrintingAreaInputProductionOrderModel(Model.Area, 1, "no", "type",1, "ins", "cat", "buyer", "const", "uin", "col", "mot", "unit", 1, 0, true, 1));
+
             var service = GetService(GetServiceProvider(repoMock.Object, movementRepoMock.Object, summaryRepoMock.Object, sppRepoMock.Object, sppoutRepoMock.Object).Object);
 
             var result = await service.ReadById(1);
