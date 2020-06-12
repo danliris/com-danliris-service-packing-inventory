9f21f6cc197b68626fc9ab2aac43fe35282ac091
achensetyawati <achenzetya@gmail.com>
2020-06-12 14:11:06 +0700

    Update GarmentLetterOfCreditViewModel.cs

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/LetterOfCredit/GarmentLetterOfCreditViewModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/LetterOfCredit/GarmentLetterOfCreditViewModel.cs
@@ -20,62 +20,67 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
         public double Quantity { get;  set; }
         public UnitOfMeasurement Uom { get;  set; }
         public double TotalAmount { get;  set; }
+        public bool available { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (string.IsNullOrEmpty(DocumentCreditNo))
             {
-                yield return new ValidationResult("Document Credit No tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("Document Credit No tidak boleh kosong", new List<string> { "DocumentCreditNo" });
+            }
+            else if (available)
+            {
+                yield return new ValidationResult("Document Credit No sudah ada", new List<string> { "DocumentCreditNo" });
             }
 
             if (Date == null || Date == DateTimeOffset.MinValue)
             {
-                yield return new ValidationResult("Tanggal tidak boleh kosong", new List<string> { "date" });
+                yield return new ValidationResult("Tanggal tidak boleh kosong", new List<string> { "Date" });
             }
 
             if (string.IsNullOrEmpty(IssuedBank))
             {
-                yield return new ValidationResult("Issued Bank tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("Issued Bank tidak boleh kosong", new List<string> { "IssuedBank" });
             }
 
             if (Applicant == null || Applicant.Id == 0)
             {
-                yield return new ValidationResult("Applicant tidak boleh kosong", new List<string> { "forwarder" });
+                yield return new ValidationResult("Applicant tidak boleh kosong", new List<string> { "Applicant" });
             }
 
             if (ExpireDate == null || ExpireDate == DateTimeOffset.MinValue)
             {
-                yield return new ValidationResult("Expire Date tidak boleh kosong", new List<string> { "date" });
+                yield return new ValidationResult("Expire Date tidak boleh kosong", new List<string> { "ExpireDate" });
             }
 
             if (string.IsNullOrEmpty(ExpirePlace))
             {
-                yield return new ValidationResult("Expire Place tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("Expire Place tidak boleh kosong", new List<string> { "ExpirePlace" });
             }
 
             if (LatestShipment == null || LatestShipment == DateTimeOffset.MinValue)
             {
-                yield return new ValidationResult("Latest Shipment tidak boleh kosong", new List<string> { "date" });
+                yield return new ValidationResult("Latest Shipment tidak boleh kosong", new List<string> { "LatestShipment" });
             }
 
             if (string.IsNullOrEmpty(LCCondition))
             {
-                yield return new ValidationResult("L/C Condition tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("L/C Condition tidak boleh kosong", new List<string> { "LCCondition" });
             }
 
             if (Uom == null || Uom.Id == 0)
             {
-                yield return new ValidationResult("Satuan tidak boleh kosong", new List<string> { "forwarder" });
+                yield return new ValidationResult("Satuan tidak boleh kosong", new List<string> { "Uom" });
             }
 
             if (Quantity<=0)
             {
-                yield return new ValidationResult("Quantity tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("Quantity tidak boleh kosong", new List<string> { "Quantity" });
             }
 
             if (TotalAmount <= 0)
             {
-                yield return new ValidationResult("Total Amount tidak boleh kosong", new List<string> { "invoiceNo" });
+                yield return new ValidationResult("Total Amount tidak boleh kosong", new List<string> { "TotalAmount" });
             }
         }
     }
