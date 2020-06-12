9ebb7c8c9d4e3e0e9ce39c8b96826793e0ff39de
achensetyawati <achenzetya@gmail.com>
2020-06-12 10:43:48 +0700

    GarmentShipping-LetterOfCredit Tests

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/GarmentShipping/GarmentLetterOfCredit/GarmentLetterOfCreditRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.LetterOfCredit;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.LetterOfCredit;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.GarmentLetterOfCredit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.GarmentShipping.GarmentLetterOfCredit
+{
+    public class GarmentLetterOfCreditRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, GarmentLetterOfCreditRepository, GarmentShippingLetterOfCreditModel, GarmentLetterOfCreditDataUtil>
+    {
+        private const string ENTITY = "GarmentLetterOfCredit";
+
+        public GarmentLetterOfCreditRepositoryTest() : base(ENTITY)
+        {
+        }
+    }
+}
