67540dead5a2e0cd479400054891399c63342dfb
achensetyawati <achenzetya@gmail.com>
2020-06-13 08:53:20 +0700

    GarmentAmendLetterOfCredit

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/AmendLetterOfCredit/GarmentAmendLetterOfCreditControllerPutTest.cs
@@ -0,0 +1,92 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.AmendLetterOfCredit;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShipping.AmendLetterOfCredit
+{
+    public class GarmentAmendLetterOfCreditControllerPutTest : GarmentAmendLetterOfCreditControllerTest
+    {
+        [Fact]
+        public async Task Put_Ok()
+        {
+            var dataUtil = ViewModel;
+
+            var serviceMock = new Mock<IGarmentAmendLetterOfCreditService>();
+            serviceMock
+                .Setup(s => s.Update(It.IsAny<int>(), It.IsAny<GarmentAmendLetterOfCreditViewModel>()))
+                .ReturnsAsync(1);
+            var service = serviceMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            validateServiceMock
+                .Setup(s => s.Validate(It.IsAny<GarmentAmendLetterOfCreditViewModel>()))
+                .Verifiable();
+            var validateService = validateServiceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+
+            var response = await controller.Put(dataUtil.Id, dataUtil);
+
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public async Task Put_ValidationException_BadRequest()
+        {
+            var dataUtil = ViewModel;
+
+            var serviceMock = new Mock<IGarmentAmendLetterOfCreditService>();
+            var service = serviceMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            validateServiceMock
+                .Setup(s => s.Validate(It.IsAny<GarmentAmendLetterOfCreditViewModel>()))
+                .Throws(GetServiceValidationExeption());
+            var validateService = validateServiceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+            var response = await controller.Put(dataUtil.Id, dataUtil);
+
+            Assert.Equal((int)HttpStatusCode.BadRequest, GetStatusCode(response));
+        }
+
+        [Fact]
+        public async Task Put_Exception_InternalServerError()
+        {
+            var dataUtil = ViewModel;
+
+            var serviceMock = new Mock<IGarmentAmendLetterOfCreditService>();
+            serviceMock
+                .Setup(s => s.Update(It.IsAny<int>(), It.IsAny<GarmentAmendLetterOfCreditViewModel>()))
+                .ThrowsAsync(new Exception());
+            var service = serviceMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            validateServiceMock
+                .Setup(s => s.Validate(It.IsAny<GarmentAmendLetterOfCreditViewModel>()))
+                .Verifiable();
+            var validateService = validateServiceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+            var response = await controller.Put(dataUtil.Id, dataUtil);
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/GarmentShipping/AmendLetterOfCredit/GarmentAmendLetterOfCreditDataUtil.cs
@@ -0,0 +1,27 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.AmendLetterOfCredit;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.AmendLetterOfCredit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.AmendLetterOfCredit
+{
+    public class GarmentAmendLetterOfCreditDataUtil : BaseDataUtil<GarmentAmendLetterOfCreditRepository, GarmentShippingAmendLetterOfCreditModel>
+    {
+        public GarmentAmendLetterOfCreditDataUtil(GarmentAmendLetterOfCreditRepository repository) : base(repository)
+        {
+        }
+
+        public override GarmentShippingAmendLetterOfCreditModel GetModel()
+        {
+            var model = new GarmentShippingAmendLetterOfCreditModel("no",1,1, DateTimeOffset.Now, "",  2);
+            return model;
+        }
+
+        public override GarmentShippingAmendLetterOfCreditModel GetEmptyModel()
+        {
+            var model = new GarmentShippingAmendLetterOfCreditModel(null,0,0, DateTimeOffset.MinValue, null, 0);
+            return model;
+        }
+    }
+}
