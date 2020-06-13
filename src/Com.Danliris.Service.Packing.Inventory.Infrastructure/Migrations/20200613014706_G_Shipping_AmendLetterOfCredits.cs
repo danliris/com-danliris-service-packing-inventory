81a19fbc353886eefd0656a78af609c058024501
achensetyawati <achenzetya@gmail.com>
2020-06-13 08:53:50 +0700

    GarmentAmendLetterOfCredit Migration

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200613014706_G_Shipping_AmendLetterOfCredits.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class G_Shipping_AmendLetterOfCredits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UomUnit",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitCode",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SCNo",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RONo",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityName",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityDesc",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityCode",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 5,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BuyerBrandName",
+                table: "GarmentShippingInvoiceItems",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "GarmentShippingAmendLetterOfCredits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    CreatedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    DeletedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    DocumentCreditNo = table.Column<string>(maxLength: 50, nullable: true),
+                    LetterOfCreditId = table.Column<int>(nullable: false),
+                    AmendNumber = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTimeOffset>(nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true),
+                    Amount = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarmentShippingAmendLetterOfCredits", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarmentShippingAmendLetterOfCredits");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UomUnit",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitCode",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SCNo",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RONo",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityName",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityDesc",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ComodityCode",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BuyerBrandName",
+                table: "GarmentShippingInvoiceItems",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
