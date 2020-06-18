563c0f581eed9297482c2d9e2ad762ab0b3ee19b
adepamungkas <ade.pamungkas9@gmail.com>
2020-06-18 09:18:00 +0700

    ProductSKUInventoryMovementRepositoryTest

--- src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/Inventory/ProductSKUInventoryDocumentRepositoryTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/Inventory/ProductSKUInventoryDocumentRepositoryTest.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Inventory;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.Inventory;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Inventory;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.Inventory
 {
-    class ProductSKUInventoryDocumentRepositoryTest
+  public  class ProductSKUInventoryDocumentRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, ProductSKUInventoryDocumentRepository, ProductSKUInventoryDocumentModel, ProductSKUInventoryDocumentDataUtil>
     {
+        
+        private const string ENTITY = "ProductSKUInventoryDocuments";
+        public ProductSKUInventoryDocumentRepositoryTest() : base(ENTITY)
+        {
+        }
     }
 }
--- src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/Inventory/ProductSKUInventorySummaryRepositoryTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/Inventory/ProductSKUInventorySummaryRepositoryTest.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Inventory;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.Inventory;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.Inventory;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.Inventory
 {
-    class ProductSKUInventorySummaryRepositoryTest
+  public  class ProductSKUInventorySummaryRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, ProductSKUInventorySummaryRepository, ProductSKUInventorySummaryModel, ProductSKUInventorySummaryDataUtil>
     {
+        
+        private const string ENTITY = "ProductSKUInventorySummaries";
+        public ProductSKUInventorySummaryRepositoryTest() : base(ENTITY)
+        {
+        }
     }
 }
