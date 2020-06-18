0f77cefa277eb468d3a6f1d22a95cc2bf7ef5442
achensetyawati <achenzetya@gmail.com>
2020-06-18 08:26:15 +0700

    GarmentShippingExportSalesDO test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/ExportSalesDO/GarmentShippingExportSalesDOControllerGetTest.cs
@@ -0,0 +1,61 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.ExportSalesDO;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
+using Com.Danliris.Service.Packing.Inventory.Application.Utilities;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShipping.ExportSalesDO
+{
+    public class GarmentShippingExportSalesDOControllerGetTest : GarmentShippingExportSalesDOControllerTest
+    {
+        [Fact]
+        public void Get_Ok()
+        {
+            var serviceMock = new Mock<IGarmentShippingExportSalesDOService>();
+            serviceMock
+                .Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new ListResult<IndexViewModel>(new List<IndexViewModel>(), 1, 1, 1));
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            var validateService = validateServiceMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+            var response = controller.Get();
+
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void Get_Exception_InternalServerError()
+        {
+            var dataUtil = ViewModel;
+
+            var serviceMock = new Mock<IGarmentShippingExportSalesDOService>();
+            serviceMock
+                .Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Throws(new Exception());
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            var validateService = validateServiceMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+            var response = controller.Get();
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Services/GarmentShipping/ExportSalesDO/GarmentShippingExportSalesDOValidationTest.cs
@@ -0,0 +1,22 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.ExportSalesDO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Services.GarmentShipping.ExportSalesDO
+{
+    public class GarmentShippingExportSalesDOValidationTest
+    {
+        [Fact]
+        public void Validate_DefaultValue()
+        {
+            GarmentShippingExportSalesDOViewModel viewModel = new GarmentShippingExportSalesDOViewModel();
+
+            var result = viewModel.Validate(null);
+            Assert.NotEmpty(result.ToList());
+        }
+
+    }
+}
