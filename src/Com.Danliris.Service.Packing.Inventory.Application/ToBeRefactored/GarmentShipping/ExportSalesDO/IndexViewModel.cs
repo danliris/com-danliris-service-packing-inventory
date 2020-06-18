b4ce37f9ad9dd43abf4162ea2341b8a3aa47f85e
achensetyawati <achenzetya@gmail.com>
2020-06-18 08:26:32 +0700

    GarmentShippingExportSalesDO

---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/ExportSalesDO/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.CommonViewModelObjectProperties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.ExportSalesDO
+{
+    public class IndexViewModel
+    {
+        public int id { get; set; }
+        public string exportSalesDONo { get; set; }
+        public string invoiceNo { get; set; }
+        public DateTimeOffset? date { get; set; }
+        public Buyer buyerAgent { get; set; }
+        public string to { get; set; }
+        public Unit unit { get; set; }
+    }
+}
