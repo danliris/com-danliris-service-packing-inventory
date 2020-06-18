e2b7471db903594771d2063a7dbd19650674905e
kuswandanu <kuswandanu@gmail.com>
2020-06-18 15:17:53 +0700

    MaterialDeliveryNote

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200618081629_MaterialDeliveryNote.cs
@@ -0,0 +1,477 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class MaterialDeliveryNote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageNumber",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SCNumber",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Receiver",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FONumber",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DONumber",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BonCode",
+                table: "MaterialDeliveryNote",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WeightDOS",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WeightCone",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WeightBruto",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WeightBale",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NoSPP",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialName",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InputLot",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GetTotal",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "Items",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageNumber",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SCNumber",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Receiver",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FONumber",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DONumber",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BonCode",
+                table: "MaterialDeliveryNote",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WeightDOS",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WeightCone",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WeightBruto",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WeightBale",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NoSPP",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialName",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedBy",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastModifiedAgent",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InputLot",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "GetTotal",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedBy",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedAgent",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAgent",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
