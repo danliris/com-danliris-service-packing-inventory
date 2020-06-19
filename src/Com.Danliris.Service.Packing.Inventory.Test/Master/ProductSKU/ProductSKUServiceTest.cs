738f42581b21ad4b4db732bdc361aef13cc09ff4
adepamungkas <ade.pamungkas9@gmail.com>
2020-06-19 09:30:32 +0700

    Update ProductSKUServiceTest.cs

--- src/Com.Danliris.Service.Packing.Inventory.Test/Master/ProductSKU/ProductSKUServiceTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Master/ProductSKU/ProductSKUServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Com.Danliris.Service.Packing.Inventory.Application.Master.ProductSKU;
+﻿using Com.Danliris.Service.Packing.Inventory.Application;
+using Com.Danliris.Service.Packing.Inventory.Application.Master.ProductSKU;
 using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
 using Com.Danliris.Service.Packing.Inventory.Data;
 using Com.Danliris.Service.Packing.Inventory.Data.Models.Product;
@@ -45,6 +46,11 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Master.ProductSKU
             {
                 return new FormDto()
                 {
+                    Code = "Code",
+                    Name ="Name",
+                    CategoryId =1,
+                    Description = "Description",
+                    UOMId =1
                 };
 
             }
@@ -74,19 +80,56 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Master.ProductSKU
             }
         }
 
+
+        private IndexQueryParam queryParam
+        {
+            get
+            {
+                return new IndexQueryParam();
+            }
+        }
         
 
-
-
         [Fact]
-        public async Task Create_Throws_ServiceValidationException()
+        public async Task Create_Return_SUccess()
         {
             var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
             var categoryRepository = new Mock<IRepository<CategoryModel>>();
             var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
 
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetName("NewName");
+            newProductSKUModel.SetCode("NewCode");
             productSKURepository.Setup(s => s.ReadAll())
-               .Returns(new List<ProductSKUModel>() { productSKUModel }.AsQueryable());
+               .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
+
+            productSKURepository.Setup(s => s.InsertAsync(It.IsAny<ProductSKUModel>()))
+              .ReturnsAsync(1);
+
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            var result = await service.Create(formDto);
+            Assert.True(0 < result);
+
+        }
+
+
+
+        [Fact]
+        public async Task Create__with_Duplicate_Name_Throws_ServiceValidationException()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetName("Name");
+            productSKURepository.Setup(s => s.ReadAll())
+               .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
 
             productSKURepository.Setup(s => s.InsertAsync(It.IsAny<ProductSKUModel>()))
               .ReturnsAsync(1);
@@ -100,6 +143,35 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Master.ProductSKU
 
             await Assert.ThrowsAsync<ServiceValidationException>(() => service.Create(formDto));
         }
+
+
+        [Fact]
+        public async Task Create_with_Duplicate_Code_Throws_ServiceValidationException()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetCode("Code");
+           
+            productSKURepository.Setup(s => s.ReadAll())
+               .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
+
+            productSKURepository.Setup(s => s.InsertAsync(It.IsAny<ProductSKUModel>()))
+              .ReturnsAsync(1);
+
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            await Assert.ThrowsAsync<ServiceValidationException>(() => service.Create(formDto));
+        }
+
+       
 
         [Fact]
         public async Task Should_Success_Delete()
@@ -150,5 +222,137 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Master.ProductSKU
             Assert.NotNull( result);
         }
 
+
+        [Fact]
+        public async Task GetById_Return_Null()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+
+            productSKURepository.Setup(s => s.ReadByIdAsync(It.IsAny<int>()))
+              .ReturnsAsync(()=>null);
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            var result = await service.GetById(1);
+            Assert.Null(result);
+        }
+
+       // [Fact]
+        public async Task GetIndex_Return_Success()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+            
+            productSKURepository.Setup(s => s.ReadAll())
+                  .Returns(new List<ProductSKUModel>() { productSKUModel }.AsQueryable());
+
+            categoryRepository.Setup(s => s.ReadAll())
+           .Returns(new List<CategoryModel>() { categoryModel }.AsQueryable());
+
+
+            unitOfMeasurementRepository.Setup(s => s.ReadAll())
+           .Returns(new List<UnitOfMeasurementModel>() { unitOfMeasurementModel }.AsQueryable());
+
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            var result = await service.GetIndex(queryParam);
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public async Task Update_Return_Success()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetCode("newCode");
+            newProductSKUModel.SetName("newName");
+            productSKURepository.Setup(s => s.ReadByIdAsync(It.IsAny<int>())).ReturnsAsync(productSKUModel);
+            productSKURepository.Setup(s => s.ReadAll())
+                  .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
+
+            productSKURepository.Setup(s => s.UpdateAsync(It.IsAny<int>(), It.IsAny<ProductSKUModel>()))
+                .ReturnsAsync(1);
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            var result = await service.Update(1,formDto);
+            Assert.True(0<result);
+        }
+
+        [Fact]
+        public async Task Update_with_duplicateCode_Throws_ServiceValidationException()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetCode("Code");
+
+            productSKURepository.Setup(s => s.ReadByIdAsync(It.IsAny<int>())).ReturnsAsync(productSKUModel);
+                  
+
+            productSKURepository.Setup(s => s.ReadAll())
+                  .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
+
+            productSKURepository.Setup(s => s.UpdateAsync(It.IsAny<int>(), It.IsAny<ProductSKUModel>()))
+                .ReturnsAsync(1);
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            await Assert.ThrowsAsync<ServiceValidationException>(() => service.Update(1, formDto));
+        }
+
+        [Fact]
+        public async Task Update_with_duplicate_Name_Throws_ServiceValidationException()
+        {
+            var productSKURepository = new Mock<IRepository<ProductSKUModel>>();
+            var categoryRepository = new Mock<IRepository<CategoryModel>>();
+            var unitOfMeasurementRepository = new Mock<IRepository<UnitOfMeasurementModel>>();
+
+            var newProductSKUModel = productSKUModel;
+            newProductSKUModel.SetName("Name");
+            productSKURepository.Setup(s => s.ReadByIdAsync(It.IsAny<int>())).ReturnsAsync(productSKUModel);
+            productSKURepository.Setup(s => s.ReadAll())
+                  .Returns(new List<ProductSKUModel>() { newProductSKUModel }.AsQueryable());
+
+            productSKURepository.Setup(s => s.UpdateAsync(It.IsAny<int>(), It.IsAny<ProductSKUModel>()))
+                .ReturnsAsync(1);
+
+            var service = GetService(GetServiceProvider(
+                productSKURepository.Object,
+                categoryRepository.Object,
+                unitOfMeasurementRepository.Object
+                ).Object);
+
+            await Assert.ThrowsAsync<ServiceValidationException>(() => service.Update(1, formDto));
+        }
     }
+    
 }
