3cc549bdf3d1ecbbf58cfca74104d32422387af8
achensetyawati <achenzetya@gmail.com>
2020-06-24 11:37:07 +0700

    ShippingInvoice pdf

---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/Utilities/HttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private HttpClient _client = new HttpClient();
+
+        public HttpClientService(IIdentityProvider identityService)
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, identityService.Token);
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _client.GetAsync(url);
+        }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/Utilities/IHttpClientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities
+{
+    public interface IHttpClientService
+    {
+        Task<HttpResponseMessage> GetAsync(string url);
+    }
+}
