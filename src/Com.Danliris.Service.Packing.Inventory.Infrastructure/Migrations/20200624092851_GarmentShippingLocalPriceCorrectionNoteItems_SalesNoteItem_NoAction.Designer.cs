16fa7945cc8141d1a7416bf4dd36f9d94c7e1d45
kuswandanu <kuswandanu@gmail.com>
2020-06-25 06:10:26 +0700

    GarmentShippingLocalPriceCorrectionNoteItems SalesNoteItem NoAction

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200624092851_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_NoAction.Designer.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200624092851_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_NoAction.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Packing.Inventory.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
 {
     [DbContext(typeof(PackingInventoryDbContext))]
-    partial class PackingInventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200624092851_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_NoAction")]
+    partial class GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_NoAction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
