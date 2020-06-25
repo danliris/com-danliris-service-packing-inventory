1d133df4fc5406a6cb5135c6d73d2aacd8fab86f
achensetyawati <achenzetya@gmail.com>
2020-06-25 09:39:11 +0700

    G-Shipping LocalReturnNote Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/GarmentShipping/LocalReturnNote/GarmentShippingLocalReturnNoteDataUtil.cs
@@ -0,0 +1,31 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.LocalReturnNote;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.LocalReturnNote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.LocalReturnNote
+{
+    public class GarmentShippingLocalReturnNoteDataUtil : BaseDataUtil<GarmentShippingLocalReturnNoteRepository, GarmentShippingLocalReturnNoteModel>
+    {
+        public GarmentShippingLocalReturnNoteDataUtil(GarmentShippingLocalReturnNoteRepository repository) : base(repository)
+        {
+        }
+
+        public override GarmentShippingLocalReturnNoteModel GetModel()
+        {
+            var items = new HashSet<GarmentShippingLocalReturnNoteItemModel> { new GarmentShippingLocalReturnNoteItemModel(1, 1) };
+            var model = new GarmentShippingLocalReturnNoteModel("", 1, DateTimeOffset.Now, "", items);
+
+            return model;
+        }
+
+        public override GarmentShippingLocalReturnNoteModel GetEmptyModel()
+        {
+            var items = new HashSet<GarmentShippingLocalReturnNoteItemModel> { new GarmentShippingLocalReturnNoteItemModel(0, 0) };
+            var model = new GarmentShippingLocalReturnNoteModel(null, 0, DateTimeOffset.MinValue,null,  items);
+
+            return model;
+        }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/GarmentShipping/LocalReturnNote/GarmentShippingLocalReturnNoteRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.LocalReturnNote;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.LocalReturnNote;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.LocalReturnNote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.GarmentShipping.LocalReturnNote
+{
+    public class GarmentShippingLocalReturnNoteRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, GarmentShippingLocalReturnNoteRepository, GarmentShippingLocalReturnNoteModel, GarmentShippingLocalReturnNoteDataUtil>
+    {
+        private const string ENTITY = "GarmentShippingLocalReturnNote";
+
+        public GarmentShippingLocalReturnNoteRepositoryTest() : base(ENTITY)
+        {
+        }
+    }
+}
