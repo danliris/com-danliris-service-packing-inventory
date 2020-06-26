8d569e715fe500d19a6de7a81eac046bf5deb995
kuswandanu <kuswandanu@gmail.com>
2020-06-26 07:25:10 +0700

    GarmentShippingLocalPriceCorrectionNoteItems SalesNoteItem Restrict

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200625174126_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_Restrict.Designer.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200625174126_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_Restrict.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Packing.Inventory.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
 {
     [DbContext(typeof(PackingInventoryDbContext))]
-    partial class PackingInventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200625174126_GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_Restrict")]
+    partial class GarmentShippingLocalPriceCorrectionNoteItems_SalesNoteItem_Restrict
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
