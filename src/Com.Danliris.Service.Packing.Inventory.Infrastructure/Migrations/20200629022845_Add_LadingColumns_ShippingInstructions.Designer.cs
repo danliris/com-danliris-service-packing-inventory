af7bd1b2fbfbcfbe29a141354902ad14ab9d323e
achensetyawati <achenzetya@gmail.com>
2020-06-29 10:16:03 +0700

    Update Shipping Instruction Migration

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200629022845_Add_LadingColumns_ShippingInstructions.Designer.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200629022845_Add_LadingColumns_ShippingInstructions.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Packing.Inventory.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
 {
     [DbContext(typeof(PackingInventoryDbContext))]
-    partial class PackingInventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200629022845_Add_LadingColumns_ShippingInstructions")]
+    partial class Add_LadingColumns_ShippingInstructions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
