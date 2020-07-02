a067df65c9d9733823a7b39498655ed30e304d59
adepamungkas <ade.pamungkas9@gmail.com>
2020-07-02 17:03:16 +0700

    FabricProductSKUCompositeStringFormDtoTest

--- src/Com.Danliris.Service.Packing.Inventory.Test/Master/Fabric/FabricProductSKUCompositeStringFormDtoTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Master/Fabric/FabricProductSKUCompositeStringFormDtoTest.cs
@@ -10,6 +10,31 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Master.Fabric
     public class FabricProductSKUCompositeStringFormDtoTest
     {
         [Fact]
+        public void Should_Success_Instantiate()
+        {
+            FabricProductSKUCompositeStringFormDto dto = new FabricProductSKUCompositeStringFormDto()
+            {
+                ColorWay = "ColorWay",
+                ConstructionType = "ConstructionType",
+                Grade= "Grade",
+                ProcessType = "ProcessType",
+                UOM = "UOM",
+                WarpThread = "WarpThread",
+                WeftThread = "WeftThread",
+                WovenType = "WovenType",
+                Width = "Width"
+            };
+            Assert.Equal("WovenType", dto.WovenType);
+            Assert.Equal("ColorWay", dto.ColorWay);
+            Assert.Equal("ConstructionType", dto.ConstructionType);
+            Assert.Equal("Grade", dto.Grade);
+            Assert.Equal("ProcessType", dto.ProcessType);
+            Assert.Equal("WarpThread", dto.WarpThread);
+            Assert.Equal("WeftThread", dto.WeftThread);
+            Assert.Equal("Width", dto.Width);
+        }
+
+        [Fact]
         public void Validate_default()
         {
             FabricProductSKUCompositeStringFormDto dto = new FabricProductSKUCompositeStringFormDto();
