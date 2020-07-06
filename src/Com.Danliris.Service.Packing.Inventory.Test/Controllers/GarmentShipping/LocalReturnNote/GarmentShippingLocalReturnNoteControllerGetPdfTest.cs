6f79cd25b99716a9ec28ad5d9125be20d0509695
achensetyawati <achenzetya@gmail.com>
2020-07-06 14:36:17 +0700

    Price Cutting Note PDF

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/LocalReturnNote/GarmentShippingLocalReturnNoteControllerGetPdfTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/LocalReturnNote/GarmentShippingLocalReturnNoteControllerGetPdfTest.cs
@@ -3,19 +3,69 @@ using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentS
 using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.ShippingLocalSalesNote;
 using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
 using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Com.Danliris.Service.Packing.Inventory.WebApi.Controllers.GarmentShipping.DebitNote;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShipping.LocalReturnNote
 {
-    public class GarmentShippingLocalReturnNoteControllerGetPdfTest : GarmentShippingLocalReturnNoteControllerTest
+    public class GarmentShippingLocalReturnNoteControllerGetPdfTest 
     {
+        protected GarmentShippingLocalReturnNoteController GetController(IGarmentShippingLocalReturnNoteService service, IIdentityProvider identityProvider, IValidateService validateService)
+        {
+            var claimPrincipal = new Mock<ClaimsPrincipal>();
+            var claims = new Claim[]
+            {
+                new Claim("username", "unittestusername")
+            };
+            claimPrincipal.Setup(claim => claim.Claims).Returns(claims);
+
+            var controller = new GarmentShippingLocalReturnNoteController(service, identityProvider, validateService)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                    {
+                        User = claimPrincipal.Object
+
+                    }
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer unittesttoken";
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = $"{It.IsAny<int>()}";
+            controller.ControllerContext.HttpContext.Request.Path = new PathString("/v1/unit-test");
+
+            return controller;
+        }
+
+
+        protected ServiceValidationException GetServiceValidationExeption()
+        {
+            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
+            List<ValidationResult> validationResults = new List<ValidationResult>()
+            {
+                new ValidationResult("message",new string[1]{ "A" }),
+                new ValidationResult("{}",new string[1]{ "B" })
+            };
+            System.ComponentModel.DataAnnotations.ValidationContext validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(ViewModel, serviceProvider.Object, null);
+            return new ServiceValidationException(validationContext, validationResults);
+        }
+
+        protected int GetStatusCode(IActionResult response)
+        {
+            return (int)response.GetType().GetProperty("StatusCode").GetValue(response, null);
+        }
+
         private Buyer buyerVm
         {
             get
--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/LocalSalesDO/GarmentShippingLocalSalesDOControllerGetPdfTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/LocalSalesDO/GarmentShippingLocalSalesDOControllerGetPdfTest.cs
@@ -2,18 +2,67 @@
 using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.LocalSalesDO;
 using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
 using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Com.Danliris.Service.Packing.Inventory.WebApi.Controllers.GarmentShipping.LocalSalesDO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShipping.LocalSalesDO
 {
-    public class GarmentShippingLocalSalesDOControllerGetPdfTest : GarmentShippingLocalSalesDOControllerTest
+    public class GarmentShippingLocalSalesDOControllerGetPdfTest 
     {
+        protected GarmentShippingLocalSalesDOController GetController(IGarmentShippingLocalSalesDOService service, IIdentityProvider identityProvider, IValidateService validateService)
+        {
+            var claimPrincipal = new Mock<ClaimsPrincipal>();
+            var claims = new Claim[]
+            {
+                new Claim("username", "unittestusername")
+            };
+            claimPrincipal.Setup(claim => claim.Claims).Returns(claims);
+
+            var controller = new GarmentShippingLocalSalesDOController(service, identityProvider, validateService)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                    {
+                        User = claimPrincipal.Object
+
+                    }
+                }
+            };
+            controller.ControllerContext.HttpContext.Request.Headers["Authorization"] = "Bearer unittesttoken";
+            controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = $"{It.IsAny<int>()}";
+            controller.ControllerContext.HttpContext.Request.Path = new PathString("/v1/unit-test");
+
+            return controller;
+        }
+
+
+        protected ServiceValidationException GetServiceValidationExeption()
+        {
+            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
+            List<ValidationResult> validationResults = new List<ValidationResult>()
+            {
+                new ValidationResult("message",new string[1]{ "A" }),
+                new ValidationResult("{}",new string[1]{ "B" })
+            };
+            System.ComponentModel.DataAnnotations.ValidationContext validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(viewModel, serviceProvider.Object, null);
+            return new ServiceValidationException(validationContext, validationResults);
+        }
+
+        protected int GetStatusCode(IActionResult response)
+        {
+            return (int)response.GetType().GetProperty("StatusCode").GetValue(response, null);
+        }
         private GarmentShippingLocalSalesDOViewModel viewModel
         {
             get
