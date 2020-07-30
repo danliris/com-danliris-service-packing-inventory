d633d5a587680ee9a118d02f6f2044846b12b1e4
adepamungkas <ade.pamungkas9@gmail.com>
2020-07-30 16:09:20 +0700

    Create IndexViewModelTest.cs

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Services/GarmentShipping/ExportSalesDO/IndexViewModelTest.cs
@@ -0,0 +1,36 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.ExportSalesDO;
+using Com.Danliris.Service.Packing.Inventory.Application.CommonViewModelObjectProperties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Services.GarmentShipping.ExportSalesDO
+{
+  public  class IndexViewModelTest
+    {
+        [Fact]
+        public void should_Success_Instantiate()
+        {
+            IndexViewModel index = new IndexViewModel()
+            {
+               buyerAgent =new Buyer(),
+                date = DateTimeOffset.Now,
+                id = 1,
+                exportSalesDONo = "exportSalesDONo",
+                invoiceNo = "invoiceNo",
+                to ="to",
+                unit=new Unit()
+            };
+
+            Assert.NotNull( index.buyerAgent);
+            Assert.NotNull(index.unit);
+            Assert.Equal(1, index.id);
+            Assert.Equal("exportSalesDONo", index.exportSalesDONo);
+            Assert.Equal("invoiceNo", index.invoiceNo);
+            Assert.Equal("to", index.to);
+            Assert.True(DateTimeOffset.MinValue < index.date);
+
+        }
+    }
+}
