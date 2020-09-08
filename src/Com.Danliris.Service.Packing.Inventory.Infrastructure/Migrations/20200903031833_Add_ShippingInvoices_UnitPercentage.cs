d9cd11b9229ac36674687188d5f2cc8201562f30
achensetyawati <setyawati@moonlay.com>
2020-09-08 11:29:17 +0700

    update Invoice Table Migration

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200903031833_Add_ShippingInvoices_UnitPercentage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class Add_ShippingInvoices_UnitPercentage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GarmentShippingInvoiceUnitPercentages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    CreatedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    DeletedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    GarmentShippingInvoiceId = table.Column<int>(nullable: false),
+                    UnitId = table.Column<int>(nullable: false),
+                    UnitCode = table.Column<string>(maxLength: 10, nullable: true),
+                    AmountPercentage = table.Column<decimal>(nullable: false),
+                    QuantityPercentage = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarmentShippingInvoiceUnitPercentages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GarmentShippingInvoiceUnitPercentages_GarmentShippingInvoices_GarmentShippingInvoiceId",
+                        column: x => x.GarmentShippingInvoiceId,
+                        principalTable: "GarmentShippingInvoices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GarmentShippingInvoiceUnitPercentages_GarmentShippingInvoiceId",
+                table: "GarmentShippingInvoiceUnitPercentages",
+                column: "GarmentShippingInvoiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarmentShippingInvoiceUnitPercentages");
+        }
+    }
+}
