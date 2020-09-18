764f8c839080f677b33336464f0aec4b60e789f0
achensetyawati <setyawati@moonlay.com>
2020-09-18 15:06:07 +0700

    local Sales Contract migration

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200918080418_Initial_LocalSalesContract.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class Initial_LocalSalesContract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GarmentShippingLocalSalesContracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedAgent = table.Column<string>(nullable: true),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    LastModifiedAgent = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(nullable: true),
+                    DeletedAgent = table.Column<string>(nullable: true),
+                    SalesContractNo = table.Column<string>(nullable: true),
+                    SalesContractDate = table.Column<DateTimeOffset>(nullable: false),
+                    TransactionTypeId = table.Column<int>(nullable: false),
+                    TransactionTypeCode = table.Column<string>(nullable: true),
+                    TransactionTypeName = table.Column<string>(nullable: true),
+                    SellerName = table.Column<string>(nullable: true),
+                    SellerPosition = table.Column<string>(nullable: true),
+                    SellerAddress = table.Column<string>(nullable: true),
+                    SellerNPWP = table.Column<string>(nullable: true),
+                    BuyerId = table.Column<int>(nullable: false),
+                    BuyerCode = table.Column<string>(nullable: true),
+                    BuyerName = table.Column<string>(nullable: true),
+                    BuyerAddress = table.Column<string>(nullable: true),
+                    BuyerNPWP = table.Column<string>(nullable: true),
+                    IsUseVat = table.Column<bool>(nullable: false),
+                    SubTotal = table.Column<decimal>(nullable: false),
+                    IsUsed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarmentShippingLocalSalesContracts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GarmentShippingLocalSalesContractItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedAgent = table.Column<string>(nullable: true),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    LastModifiedAgent = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(nullable: true),
+                    DeletedAgent = table.Column<string>(nullable: true),
+                    LocalSalesContractId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    ProductCode = table.Column<string>(nullable: true),
+                    ProductName = table.Column<string>(nullable: true),
+                    Quantity = table.Column<double>(nullable: false),
+                    UomId = table.Column<int>(nullable: false),
+                    UomUnit = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    GarmentShippingLocalSalesContractModelId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarmentShippingLocalSalesContractItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GarmentShippingLocalSalesContractItems_GarmentShippingLocalSalesContracts_GarmentShippingLocalSalesContractModelId",
+                        column: x => x.GarmentShippingLocalSalesContractModelId,
+                        principalTable: "GarmentShippingLocalSalesContracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GarmentShippingLocalSalesContractItems_GarmentShippingLocalSalesContractModelId",
+                table: "GarmentShippingLocalSalesContractItems",
+                column: "GarmentShippingLocalSalesContractModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarmentShippingLocalSalesContractItems");
+
+            migrationBuilder.DropTable(
+                name: "GarmentShippingLocalSalesContracts");
+        }
+    }
+}
