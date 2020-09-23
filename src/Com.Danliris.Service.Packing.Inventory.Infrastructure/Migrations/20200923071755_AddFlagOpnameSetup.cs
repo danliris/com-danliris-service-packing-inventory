09151847f9ba1e5551f0c5c6d187a81f702df487
skydivine <17161989+skydivine@users.noreply.github.com>
2020-09-23 14:20:00 +0700

    AddColumnOpnameInput

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20200923071755_AddFlagOpnameSetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class AddFlagOpnameSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "OpnameInput",
+                table: "DyeingPrintingAreaInputs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OpnameInput",
+                table: "DyeingPrintingAreaInputs");
+        }
+    }
+}
