377cd0a6152f83a4ad579dcba5cd3af1a69b5cc8
adepamungkas <ade.pamungkas9@gmail.com>
2020-10-08 11:02:46 +0700

    Update DyeingPrintingAreaInputProductionOrderRepositoryTest.cs

--- src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/DyeingPrintingAreaInputProductionOrderRepositoryTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/DyeingPrintingAreaInputProductionOrderRepositoryTest.cs
@@ -330,6 +330,24 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories
         }
 
         [Fact]
+        public  async Task Should_Success_UpdateDateOutsync()
+        {
+            string testName = GetCurrentMethod();
+            var dbContext = DbContext(testName);
+
+            var serviceProvider = GetServiceProviderMock(dbContext);
+
+            var repo = new DyeingPrintingAreaInputProductionOrderRepository(dbContext, serviceProvider.Object);
+            var repo2 = new DyeingPrintingAreaInputProductionOrderRepository(dbContext, serviceProvider.Object);
+            var emptyData = DataUtil(repo, dbContext).GetTestData();
+            
+            var result = await repo2.UpdateDateOutsync(1, DateTimeOffset.Now);
+
+            Assert.NotEqual(0, result);
+        }
+
+
+        [Fact]
         public virtual async Task Should_Success_UpdatePackingFromOut2()
         {
             string testName = GetCurrentMethod() + "UpdatePackingFromOut2";
