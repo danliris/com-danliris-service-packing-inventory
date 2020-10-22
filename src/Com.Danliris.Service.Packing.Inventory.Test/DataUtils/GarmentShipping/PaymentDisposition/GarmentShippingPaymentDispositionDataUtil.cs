04ba5236e0fa7c6b232658ddf395cd070b914683
achensetyawati <setyawati@moonlay.com>
2020-10-22 10:07:01 +0700

    PaymentDisposition Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/GarmentShipping/PaymentDisposition/GarmentShippingPaymentDispositionDataUtil.cs
@@ -0,0 +1,35 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.PaymentDisposition;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.PaymentDisposition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.PaymentDisposition
+{
+    public class GarmentShippingPaymentDispositionDataUtil : BaseDataUtil<GarmentShippingPaymentDispositionRepository, GarmentShippingPaymentDispositionModel>
+    {
+        public GarmentShippingPaymentDispositionDataUtil(GarmentShippingPaymentDispositionRepository repository) : base(repository)
+        {
+        }
+
+        public override GarmentShippingPaymentDispositionModel GetModel()
+        {
+            var bills = new HashSet<GarmentShippingPaymentDispositionBillDetailModel> { new GarmentShippingPaymentDispositionBillDetailModel("",1) };
+            var units = new HashSet<GarmentShippingPaymentDispositionUnitChargeModel> { new GarmentShippingPaymentDispositionUnitChargeModel(1, "",1,1) };
+            var invoices = new HashSet<GarmentShippingPaymentDispositionInvoiceDetailModel> { new GarmentShippingPaymentDispositionInvoiceDetailModel("",1,1,1,1,1,1,1) };
+            var model = new GarmentShippingPaymentDispositionModel("", "", "", "", "", 1, "", "", "", 1, "", "", 1, "", "", 1, "", "", "", "", "", DateTimeOffset.Now, "", 1, 1, 1, "", 1, 1, 1, DateTimeOffset.Now, "", "", true, "", "", DateTimeOffset.Now, "", invoices, bills, units);
+
+            return model;
+        }
+
+        public override GarmentShippingPaymentDispositionModel GetEmptyModel()
+        {
+            var bills = new HashSet<GarmentShippingPaymentDispositionBillDetailModel> { new GarmentShippingPaymentDispositionBillDetailModel(null, 0) };
+            var units = new HashSet<GarmentShippingPaymentDispositionUnitChargeModel> { new GarmentShippingPaymentDispositionUnitChargeModel(0, null, 0, 0) };
+            var invoices = new HashSet<GarmentShippingPaymentDispositionInvoiceDetailModel> { new GarmentShippingPaymentDispositionInvoiceDetailModel(null, 0, 0, 0, 0, 0, 0, 0) };
+            var model = new GarmentShippingPaymentDispositionModel(null, null, null, null, null, 0, null, null, null, 0, null, null, 0, null, null, 0, null, null, null, null, null, DateTimeOffset.MinValue, null, 0, 0, 0, null, 0, 0, 0, DateTimeOffset.MinValue, null, null, true, null, null, DateTimeOffset.MinValue, null, invoices, bills, units);
+
+            return model;
+        }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/GarmentShipping/PaymentDisposition/GarmentShippingPaymentDispositionRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.PaymentDisposition;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.PaymentDisposition;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.PaymentDisposition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.GarmentShipping.PaymentDisposition
+{
+    public class GarmentShippingPaymentDispositionRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, GarmentShippingPaymentDispositionRepository, GarmentShippingPaymentDispositionModel, GarmentShippingPaymentDispositionDataUtil>
+    {
+        private const string ENTITY = "GarmentShippingPaymentDisposition";
+
+        public GarmentShippingPaymentDispositionRepositoryTest() : base(ENTITY)
+        {
+        }
+
+        
+    }
+}
