e6cc38af2f517424aea864cd14d02c393561680a
ANDRI PURNOMO <andrea.kinan@gmail.com>
2021-01-05 00:45:45 +0700

    Create - Garment Local Sales Book
    - Create Service Garment Local Sales Book
    - Enhancement PDF Template

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Repositories/GarmentShipping/LocalReturnNote/IGarmentShippingLocalReturnNoteRepository.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Repositories/GarmentShipping/LocalReturnNote/IGarmentShippingLocalReturnNoteRepository.cs
@@ -2,11 +2,13 @@
 using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.LocalReturnNote;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.LocalReturnNote
 {
     public interface IGarmentShippingLocalReturnNoteRepository : IRepository<GarmentShippingLocalReturnNoteModel>
     {
+        IQueryable<GarmentShippingLocalReturnNoteItemModel> ReadItemAll();
     }
 }
