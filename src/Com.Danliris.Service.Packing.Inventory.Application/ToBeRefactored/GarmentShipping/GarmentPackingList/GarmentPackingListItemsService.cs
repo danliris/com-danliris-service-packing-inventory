fd1eb8b75b971284ca5398c64897de9600b05212
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-02-17 10:41:42 +0700

    Update GarmentPackingListItemsService.cs
    Fix CodeCov #2

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListItemsService.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListItemsService.cs
@@ -369,12 +369,12 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
 
                     foreach (var detail in item.Details.Where(w => w.Id == 0))
                     {
+                        detail.SetNetNetWeight(detail.NetNetWeight == 0 ? 0.9 * detail.NetWeight : detail.NetNetWeight, _identityProvider.Username, UserAgent);
                         detail.FlagForCreate(_identityProvider.Username, UserAgent);
                         foreach (var size in detail.Sizes)
                         {
                             size.FlagForCreate(_identityProvider.Username, UserAgent);
                         }
-                        detail.SetNetNetWeight(detail.NetNetWeight == 0 ? 0.9 * detail.NetWeight : detail.NetNetWeight, _identityProvider.Username, UserAgent);
                         itemToUpdate.Details.Add(detail);
                     }
                 }
