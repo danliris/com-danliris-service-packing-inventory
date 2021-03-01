67c22728495ab3902f9ece320555f266f27ae6ef
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-03-01 13:56:14 +0700

    Fix CodeCov #1

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Models/Garmentshipping/GarmentShippingPaymentDispositionRecap/GarmentShippingPaymentDispositionRecapItemModelTest.cs
@@ -0,0 +1,18 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.PaymentDispositionRecap;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Models.Garmentshipping.GarmentShippingPaymentDispositionRecap
+{
+    public class GarmentShippingPaymentDispositionRecapItemModelTest
+    {
+        [Fact]
+        public void should_Success_Instantiate()
+        {
+            GarmentShippingPaymentDispositionRecapItemModel model = new GarmentShippingPaymentDispositionRecapItemModel();
+            Assert.NotNull(model);
+        }
+    }
+}
