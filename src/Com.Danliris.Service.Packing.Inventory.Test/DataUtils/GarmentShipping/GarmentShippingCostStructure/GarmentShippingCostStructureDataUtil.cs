69305905995212cf452de312004679ec70e5f0c6
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-03-17 11:12:42 +0700

    Add Unit Test GarmentShippingCostStructure

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/DataUtils/GarmentShipping/GarmentShippingCostStructure/GarmentShippingCostStructureDataUtil.cs
@@ -0,0 +1,43 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentShippingCostStructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.GarmentShippingCostStructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.GarmentShippingCostStructure
+{
+    public class GarmentShippingCostStructureDataUtil : BaseDataUtil<GarmentShippingCostStructureRepository, GarmentShippingCostStructureModel>
+    {
+        public GarmentShippingCostStructureDataUtil(GarmentShippingCostStructureRepository repository) : base(repository)
+        {
+        }
+
+        public override GarmentShippingCostStructureModel GetModel()
+        {
+            var model = new GarmentShippingCostStructureModel("invoiceno", DateTimeOffset.Now, 1, "comodityCode", "comodityName", "hsCode", "destination", 1, "fabricType", 10000);
+
+            return model;
+        }
+
+        public GarmentShippingCostStructureModel GetModels()
+        {
+            var model = new GarmentShippingCostStructureModel("invoiceno", DateTimeOffset.Now, 1, "comodityCode", "comodityName", "hsCode", "destination", 1, "fabricType", 10000);
+
+            return model;
+        }
+
+        public override GarmentShippingCostStructureModel GetEmptyModel()
+        {
+            var model = new GarmentShippingCostStructureModel(null, DateTimeOffset.MinValue, 0, null, null, null, null, 0, null, 0);
+
+            return model;
+        }
+
+        public GarmentShippingCostStructureModel CopyModel(GarmentShippingCostStructureModel om)
+        {
+            var model = new GarmentShippingCostStructureModel(om.InvoiceNo, om.Date, om.ComodityId, om.ComodityCode, om.ComodityName, om.HsCode, om.Destination, om.FabricTypeId, om.FabricType, om.Amount){ Id = om.Id };
+
+            return model;
+        }
+    }
+}
