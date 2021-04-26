147891bdb67f5f8ff5b23e993f72b338d53c5744
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-04-26 10:30:18 +0700

    Change Format PDF GarmentShippingInvoice & Change Data Type Description

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20210426030551_ChangeColumnType_Description_ToText_GarmentShippingInvoice.Designer.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20210426030551_ChangeColumnType_Description_ToText_GarmentShippingInvoice.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Packing.Inventory.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
 {
     [DbContext(typeof(PackingInventoryDbContext))]
-    partial class PackingInventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210426030551_ChangeColumnType_Description_ToText_GarmentShippingInvoice")]
+    partial class ChangeColumnType_Description_ToText_GarmentShippingInvoice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
