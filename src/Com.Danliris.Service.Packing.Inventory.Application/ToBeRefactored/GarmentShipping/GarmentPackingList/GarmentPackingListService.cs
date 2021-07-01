2fb5b599352c0d50e7289d8fee150e0cd97555e1
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-07-01 11:02:25 +0700

    Enhancement Feature Print Excel By Carton Draft PL

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListService.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListService.cs
@@ -606,6 +606,24 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
             return new MemoryStreamResult(stream, "Packing List " + data.InvoiceNo + ".xls");
         }
 
+        public virtual async Task<MemoryStreamResult> ReadExcelByIdFilterCarton(int id)
+        {
+            var data = await _packingListRepository.ReadByIdAsync(id);
+
+            var ExcelTemplate = new GarmentPackingListExcelTemplate(_identityProvider);
+            var fob = _invoiceRepository.ReadAll().Where(w => w.PackingListId == data.Id).Select(s => s.CPrice == "FOB" ? s.From : s.To).FirstOrDefault();
+            var cPrice = _invoiceRepository.ReadAll().Where(w => w.PackingListId == data.Id).Select(s => s.CPrice).FirstOrDefault();
+
+            var viewModel = MapToViewModel(data);
+            viewModel.ShippingMarkImageFile = await _azureImageService.DownloadImage(IMG_DIR, viewModel.ShippingMarkImagePath);
+            viewModel.SideMarkImageFile = await _azureImageService.DownloadImage(IMG_DIR, viewModel.SideMarkImagePath);
+            viewModel.RemarkImageFile = await _azureImageService.DownloadImage(IMG_DIR, viewModel.RemarkImagePath);
+
+            var stream = ExcelTemplate.GenerateExcelTemplate(viewModel, fob, cPrice);
+
+            return new MemoryStreamResult(stream, "Packing List " + data.InvoiceNo + ".xls");
+        }
+
         public async Task SetPost(List<int> ids)
         {
             var models = _packingListRepository.Query.Where(m => ids.Contains(m.Id));
