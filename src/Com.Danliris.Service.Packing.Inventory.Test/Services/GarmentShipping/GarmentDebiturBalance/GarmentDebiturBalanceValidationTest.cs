30331dbf719b61d7f4d4b7b01b4ddf11c03cad4f
Leslie Aula <leslie.aula@moonlay.com>
2021-08-05 12:21:06 +0800

    Copy Garment Debitur Balance

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Services/GarmentShipping/GarmentDebiturBalance/GarmentDebiturBalanceValidationTest.cs
@@ -0,0 +1,34 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.CommonViewModelObjectProperties;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.CommonViewModelObjectProperties;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.GarmentDebiturBalance;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Services.GarmentShipping.GarmentDebiturBalance
+{
+    public class GarmentDebiturBalanceValidationTest
+    {
+        [Fact]
+        public void Validate_DefaultValue()
+        {
+            GarmentDebiturBalanceViewModel viewModel = new GarmentDebiturBalanceViewModel();
+
+            var result = viewModel.Validate(null);
+            Assert.NotEmpty(result.ToList());
+        }
+
+        [Fact]
+        public void Validate_EmptyValue()
+        {
+            GarmentDebiturBalanceViewModel viewModel = new GarmentDebiturBalanceViewModel
+            {
+                balanceDate = DateTimeOffset.MinValue,
+                buyerAgent = new BuyerAgent { Id = 0 },
+            };
+
+            var result = viewModel.Validate(null);
+            Assert.NotEmpty(result.ToList());
+        }
+    }
+}
