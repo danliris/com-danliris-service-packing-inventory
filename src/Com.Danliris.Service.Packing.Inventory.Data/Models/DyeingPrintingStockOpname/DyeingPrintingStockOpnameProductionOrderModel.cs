9a1171222b23e5e4d07c3baec954d11b0aa49126
ichwankm <ichwankm@gmail.com>
2021-10-05 07:58:58 +0700

    Bug Fix Create Multiple Stock Opname at once

--- src/Com.Danliris.Service.Packing.Inventory.Data/Models/DyeingPrintingStockOpname/DyeingPrintingStockOpnameProductionOrderModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Data/Models/DyeingPrintingStockOpname/DyeingPrintingStockOpnameProductionOrderModel.cs
@@ -116,7 +116,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.DyeingPrintingStock
         public DyeingPrintingStockOpnameProductionOrderModel(double balance, int buyerId, string buyer, string color, string construction, string documentNo, string grade, int materialConstructionId, string materialConstructionName, int materialId,
             string materialName, string materialWidth, string motif, string packingInstruction, decimal packagingQty, double packagingLength, string packagingType, string packagingUnit,
             long productionOrderId, string productionOrderNo, string productionOrderType, double productionOrderOrderQuantity, int processTypeId, string processTypeName, int yarnMaterialId, string yarnMaterialName,
-             string remark, string status, string unit, string uomUnit, bool isStockOpname, string packingCodes, int productSKUId, int fabricSKUId, string productSKUCode, int productPackingId, int fabricPackingId )
+             string remark, string status, string unit, string uomUnit, bool isStockOpname, string packingCodes, int productSKUId, int fabricSKUId, string productSKUCode, int productPackingId, int fabricPackingId, bool hasPrintingProductSKU)
         {
             Balance = balance;
             BuyerId = buyerId;
@@ -156,6 +156,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.DyeingPrintingStock
             ProductSKUCode = productSKUCode;
             ProductPackingId = productPackingId;
             FabricPackingId = fabricPackingId;
+            HasPrintingProductSKU = hasPrintingProductSKU;
         }
 
         public DyeingPrintingStockOpnameProductionOrderModel()
