14651ec89c6f203c316336d00eb89f8e91014996
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2022-01-14 16:07:27 +0700

    Fix Unit Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Models/Garmentshipping/GarmentPackingList/GarmentPackingListModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentPackingList;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Models.Garmentshipping.GarmentPackingList
+{
+    public class GarmentPackingListModelTest
+    {
+        [Fact]
+        public void should_Success_Instantiate()
+        {
+            GarmentPackingListModel model = new GarmentPackingListModel();
+            model.SetIsShipping(true, "asd", "asd");
+            Assert.Equal(true, model.IsShipping);
+            Assert.NotNull(model);
+        }
+    }
+}
