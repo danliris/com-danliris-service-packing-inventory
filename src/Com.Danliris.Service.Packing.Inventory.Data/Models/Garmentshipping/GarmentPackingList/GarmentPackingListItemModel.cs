0b3cbc8a9dfad19e409238fccff5672cacf20464
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2022-01-27 14:39:53 +0700

    Enhancement PackingList MD

--- src/Com.Danliris.Service.Packing.Inventory.Data/Models/Garmentshipping/GarmentPackingList/GarmentPackingListItemModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Data/Models/Garmentshipping/GarmentPackingList/GarmentPackingListItemModel.cs
@@ -41,6 +41,8 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.Gar
 
         public string Remarks { get; private set; }
 
+        public string RoType { get; private set; }
+
         public ICollection<GarmentPackingListDetailModel> Details { get; private set; }
 
         public GarmentPackingListItemModel()
@@ -48,7 +50,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.Gar
             Details = new HashSet<GarmentPackingListDetailModel>();
         }
 
-        public GarmentPackingListItemModel(string rONo, string sCNo, int buyerBrandId, string buyerBrandName, int comodityId, string comodityCode, string comodityName, string comodityDescription, double quantity, int uomId, string uomUnit, double priceRO, double price, double priceFob, double priceCmt, double amount, string valas, int unitId, string unitCode, string article, string orderNo, string description, string descriptionMd, string remarks, ICollection<GarmentPackingListDetailModel> details)
+        public GarmentPackingListItemModel(string rONo, string sCNo, int buyerBrandId, string buyerBrandName, int comodityId, string comodityCode, string comodityName, string comodityDescription, double quantity, int uomId, string uomUnit, double priceRO, double price, double priceFob, double priceCmt, double amount, string valas, int unitId, string unitCode, string article, string orderNo, string description, string descriptionMd, string remarks, string roType, ICollection<GarmentPackingListDetailModel> details)
         {
             RONo = rONo;
             SCNo = sCNo;
@@ -75,6 +77,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.Gar
             DescriptionMd = descriptionMd;
             Remarks = remarks;
             Details = details;
+            RoType = roType;
         }
 
         public void SetRONo(string rONo, string userName, string userAgent)
@@ -267,6 +270,15 @@ namespace Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.Gar
             if (Remarks != remarks)
             {
                 Remarks = remarks; this.FlagForUpdate(userName, userAgent);
+            }
+        }
+
+        public void SetRoType(string roType, string userName, string userAgent)
+        {
+            if (RoType != roType)
+            {
+                RoType = roType;
+                this.FlagForUpdate(userName, userAgent);
             }
         }
 
