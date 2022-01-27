35968a4b50be889d7783bb3b95a505ce1b4e556c
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2022-01-27 16:27:11 +0700

    FIx Unit Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentPackingList/GarmentPackingListControllerDeliveredTest.cs
@@ -0,0 +1,60 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.GarmentShipping.GarmentPackingList;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Utilities;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShipping.GarmentPackingList
+{
+    public class GarmentPackingListControllerDeliveredTest : GarmentPackingListControllerTest
+    {
+        [Fact]
+        public async Task Set_Delivered_Success()
+        {
+            var serviceMock = new Mock<IGarmentPackingListService>();
+            serviceMock
+                .Setup(s => s.SetSampleDelivered(It.IsAny<List<int>>()))
+                .Verifiable();
+            var service = serviceMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            var validateService = validateServiceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+
+            var response = await controller.DeliveredSample(It.IsAny<List<int>>());
+
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public async Task Set_Delivered_Exception_InternalServerError()
+        {
+            var serviceMock = new Mock<IGarmentPackingListService>();
+            serviceMock
+                .Setup(s => s.SetSampleDelivered(It.IsAny<List<int>>()))
+                .ThrowsAsync(new Exception());
+            var service = serviceMock.Object;
+
+            var validateServiceMock = new Mock<IValidateService>();
+            var validateService = validateServiceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider, validateService);
+
+            var response = await controller.DeliveredSample(It.IsAny<List<int>>());
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+    }
+}
