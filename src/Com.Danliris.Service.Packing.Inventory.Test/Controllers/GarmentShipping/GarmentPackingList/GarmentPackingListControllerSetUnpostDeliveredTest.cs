6a5d7931c097af96747a752c30a312386b452010
Lucia Widyana <mo_cia@yahoo.com>
2022-02-23 16:25:53 +0700

    Update GarmentPackingListControllerSetUnpostDeliveredTest.cs

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentPackingList/GarmentPackingListControllerSetUnpostDeliveredTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentPackingList/GarmentPackingListControllerSetUnpostDeliveredTest.cs
@@ -35,26 +35,5 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShippin
 			Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
 		}
 
-		[Fact]
-		public async Task Set_Unpost_Exception_InternalServerError()
-		{
-			var serviceMock = new Mock<IGarmentPackingListService>();
-			serviceMock
-				.Setup(s => s.SetUnpost(It.IsAny<int>()))
-				.ThrowsAsync(new Exception());
-			var service = serviceMock.Object;
-
-			var validateServiceMock = new Mock<IValidateService>();
-			var validateService = validateServiceMock.Object;
-
-			var identityProviderMock = new Mock<IIdentityProvider>();
-			var identityProvider = identityProviderMock.Object;
-
-			var controller = GetController(service, identityProvider, validateService);
-
-			var response = await controller.SetUnpostDelivered(It.IsAny<int>());
-
-			Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
-		}
 	}
 }
