01b2166e485ca71d5be9cff173976c4803105f51
Lucia Widyana <mo_cia@yahoo.com>
2022-03-07 13:30:55 +0700

    Unit Test

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentShippingInvoice/GarmentShippingInvoiceControllerGetTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentShippingInvoice/GarmentShippingInvoiceControllerGetTest.cs
@@ -68,5 +68,32 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShippin
 
 			Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
 		}
+
+		[Fact]
+		public void GetShippingInvoiceByPackingListId()
+		{
+			var serviceMock = new Mock<IGarmentShippingInvoiceService>();
+			serviceMock
+				.Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(new ListResult<GarmentShippingInvoiceViewModel>(new List<GarmentShippingInvoiceViewModel>(), 1, 1, 1));
+			var service = serviceMock.Object;
+
+			var packingListServiceMock = new Mock<IGarmentPackingListService>();
+			packingListServiceMock
+				.Setup(s => s.ReadById(It.IsAny<int>()))
+				.ReturnsAsync(new GarmentPackingListViewModel());
+			var packingListService = packingListServiceMock.Object;
+
+			var identityProviderMock = new Mock<IIdentityProvider>();
+			var identityProvider = identityProviderMock.Object;
+
+			var validateServiceMock = new Mock<IValidateService>();
+			var validateService = validateServiceMock.Object;
+
+			var controller = GetController(service, packingListService, identityProvider, validateService);
+			var response = controller.GetShippingInvoiceByPLId(1);
+
+			Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+		}
 	}
 }
