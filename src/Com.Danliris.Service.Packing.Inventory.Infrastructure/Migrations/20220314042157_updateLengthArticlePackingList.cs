ae280eef06389674dc4a053cce5eac4b35c86fee
Lucia Wilujeng Widyana <mo_cia@yahoo.com>
2022-03-14 11:23:59 +0700

    UPDATE ARTICLE LENGTH

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20220314042157_updateLengthArticlePackingList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class updateLengthArticlePackingList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Article",
+                table: "GarmentPackingListItems",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Article",
+                table: "GarmentPackingListItems",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+    }
+}
