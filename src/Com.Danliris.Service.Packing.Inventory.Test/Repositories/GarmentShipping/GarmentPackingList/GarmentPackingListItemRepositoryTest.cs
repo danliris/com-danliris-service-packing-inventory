8988b8300369a8afe44720821ce1af3011603e58
supri0343 <supri0343@gmail.com>
2022-04-08 14:20:31 +0700

    Update Unit Test

---
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Repositories/GarmentShipping/GarmentPackingList/GarmentPackingListItemRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentPackingList;
+using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentShippingInvoice;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.GarmentPackingList;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.GarmentShippingInvoice;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.GarmentPackingList;
+using Com.Danliris.Service.Packing.Inventory.Test.DataUtils.GarmentShipping.GarmentShippingInvoice;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+namespace Com.Danliris.Service.Packing.Inventory.Test.Repositories.GarmentShipping.GarmentPackingList
+{
+    public class GarmentPackingListItemRepositoryTest : BaseRepositoryTest<PackingInventoryDbContext, GarmentPackingListItemRepository, GarmentPackingListItemModel, GarmentPackingListItemDataUtil>
+    {
+        private const string ENTITY = "GarmentPackingListItemRepositoryTest";
+
+        public GarmentPackingListItemRepositoryTest() : base(ENTITY)
+        {
+        }
+
+    }
+}
