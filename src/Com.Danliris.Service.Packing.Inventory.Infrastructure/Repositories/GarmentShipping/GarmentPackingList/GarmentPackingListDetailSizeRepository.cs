f6fe14e8844657a397baa4afcd89906308aefcb8
supri0343 <supri0343@gmail.com>
2022-04-08 11:08:57 +0700

    Tambah_MonitoringDeliveryPackingListSample

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Repositories/GarmentShipping/GarmentPackingList/GarmentPackingListDetailSizeRepository.cs
@@ -0,0 +1,64 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentShippingInvoice;
+using Com.Danliris.Service.Packing.Inventory.Infrastructure.IdentityProvider;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Com.Moonlay.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Linq;
+using Com.Danliris.Service.Packing.Inventory.Data;
+using Com.Danliris.Service.Packing.Inventory.Data.Models.Garmentshipping.GarmentPackingList;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.GarmentShipping.GarmentPackingList
+{
+    public class GarmentPackingListDetailSizeRepository : IGarmentPackingListDetailSizeRepository
+    {
+        private const string USER_AGENT = "Repository";
+        private readonly PackingInventoryDbContext _dbContext;
+        private readonly DbSet<GarmentPackingListDetailSizeModel> _dbSet;
+        private readonly IIdentityProvider _identityProvider;
+
+        public GarmentPackingListDetailSizeRepository(PackingInventoryDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<GarmentPackingListDetailSizeModel>();
+            _identityProvider = serviceProvider.GetService<IIdentityProvider>();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            var model = _dbSet
+               .FirstOrDefault(s => s.Id == id);
+
+            model.FlagForDelete(_identityProvider.Username, USER_AGENT);
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task<int> InsertAsync(GarmentPackingListDetailSizeModel model)
+        {
+            model.FlagForCreate(_identityProvider.Username, USER_AGENT);
+
+            _dbSet.Add(model);
+
+            return _dbContext.SaveChangesAsync();
+        }
+
+        public Task<GarmentPackingListDetailSizeModel> ReadByIdAsync(int id)
+        {
+            return _dbSet
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public IQueryable<GarmentPackingListDetailSizeModel> ReadAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+
+        public Task<int> UpdateAsync(int id, GarmentPackingListDetailSizeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
