1080dd9b4d4661b9192f6d83668e77bc3ab3a2a8
Lucia Widyana <mo_cia@yahoo.com>
2022-04-29 09:46:04 +0700

    unit test

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentPackingList/GarmentPackingListControllerGetTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/GarmentShipping/GarmentPackingList/GarmentPackingListControllerGetTest.cs
@@ -56,5 +56,47 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers.GarmentShippin
 
             Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
         }
-    }
+
+		[Fact]
+		public void GetPLSampleRO_Ok()
+		{
+			var serviceMock = new Mock<IGarmentPackingListService>();
+			serviceMock
+				.Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(new ListResult<GarmentPackingListViewModel>(new List<GarmentPackingListViewModel>(), 1, 1, 1));
+			var service = serviceMock.Object;
+
+			var identityProviderMock = new Mock<IIdentityProvider>();
+			var identityProvider = identityProviderMock.Object;
+
+			var validateServiceMock = new Mock<IValidateService>();
+			var validateService = validateServiceMock.Object;
+
+			var controller = GetController(service, identityProvider, validateService);
+			var response = controller.GetPLSampleRO();
+
+			Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+		}
+
+		[Fact]
+		public void GetPLSampleStyle_Ok()
+		{
+			var serviceMock = new Mock<IGarmentPackingListService>();
+			serviceMock
+				.Setup(s => s.Read(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(new ListResult<GarmentPackingListViewModel>(new List<GarmentPackingListViewModel>(), 1, 1, 1));
+			var service = serviceMock.Object;
+
+			var identityProviderMock = new Mock<IIdentityProvider>();
+			var identityProvider = identityProviderMock.Object;
+
+			var validateServiceMock = new Mock<IValidateService>();
+			var validateService = validateServiceMock.Object;
+
+			var controller = GetController(service, identityProvider, validateService);
+			var response = controller.GetPLSampleStyle(It.IsAny<string>());
+
+			Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+		}
+	}
 }
