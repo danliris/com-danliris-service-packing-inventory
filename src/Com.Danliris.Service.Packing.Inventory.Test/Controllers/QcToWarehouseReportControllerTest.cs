e15e3b9d84de0836c0f9add95f8c667437dbd29d
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-08 16:14:16 +0700

    perubahan filter tgl

--- src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/QcToWarehouseReportControllerTest.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Test/Controllers/QcToWarehouseReportControllerTest.cs
@@ -81,5 +81,66 @@ namespace Com.Danliris.Service.Packing.Inventory.Test.Controllers
 
             Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
         }
+
+        [Fact]
+        public void Should_Exception_Get()
+        {
+            var dataUtil = ViewModel;
+            //v
+            var serviceMock = new Mock<IQcToWarehouseReportService>();
+            serviceMock.Setup(s => s.GetReportData(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>()))
+                .Throws(new Exception());
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider);
+
+            var response = controller.GetReport(It.IsAny<DateTime>(), It.IsAny<DateTime>());
+
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void Should_Success_GetXls()
+        {
+            //v
+            var serviceMock = new Mock<IQcToWarehouseReportService>();
+            serviceMock.Setup(s => s.GenerateExcel(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>()))
+                .Returns(new MemoryStream());
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider);
+            //controller.ModelState.IsValid == false;
+            var response = controller.GetXls(It.IsAny<DateTime>(), It.IsAny<DateTime>());
+
+            Assert.NotNull(response);
+        }
+
+        [Fact]
+        public void Should_Exception_GetXls()
+        {
+            //v
+            var serviceMock = new Mock<IQcToWarehouseReportService>();
+            serviceMock.Setup(s => s.GenerateExcel(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<int>()))
+               .Throws(new Exception());
+            var service = serviceMock.Object;
+
+            var identityProviderMock = new Mock<IIdentityProvider>();
+            var identityProvider = identityProviderMock.Object;
+
+            var controller = GetController(service, identityProvider);
+            //controller.ModelState.IsValid == false;
+            var response = controller.GetXls(It.IsAny<DateTime>(), It.IsAny<DateTime>());
+
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+
     }
+
 }
