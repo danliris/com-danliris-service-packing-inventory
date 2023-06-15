31520ed4328f8dc32a8c2b0d199d0338fc0feae6
achensetyawati <achenzetya@gmail.com>
2023-06-15 08:40:42 +0700

    order status report

---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingReport/OrderStatusReport/IOrderStatusReportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.DyeingPrintingReport.OrderStatusReport
+{
+    public interface IOrderStatusReportService
+    {
+        List<OrderStatusReportViewModel> GetReportData(string orderType, string year);
+        MemoryStream GenerateExcel(string orderType, string year);
+    }
+}
