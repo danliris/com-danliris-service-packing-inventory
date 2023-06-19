b0c6e27547cbb2a7a664557c4b01eac97f965742
achensetyawati <achenzetya@gmail.com>
2023-06-19 16:50:14 +0700

    Create SalesDataSetting.cs

---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/Helper/SalesDataSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.Helper
+{
+    public class SalesDataSetting
+    {
+        public static string Endpoint { get; set; }
+
+        public static string TokenEndpoint { get; set; }
+
+        public static string Password { get; set; }
+
+        public static string Username { get; set; }
+    }
+}
