04ed99445c93a30024f038f911dbebafe6c35119
ardana96 <ardanagama@gmail.com>
2023-06-21 15:40:54 +0700

    Inv Transaction - Detail Receipt Warehouse and Monitoring

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingWarehouse/IN/ViewModel/DPInputWarehouseItemCreateViewModel.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingWarehouse/IN/ViewModel/DPInputWarehouseItemCreateViewModel.cs
@@ -49,6 +49,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Dyei
         public int ProductPackingId { get; set; }
         public int FabricPackingId { get; set; }
         public string ProductPackingCode { get; set; }
+        public double Balance { get; set; }
       
         public string InventoryType { get; set; }
         public string MaterialOrigin { get; set; }
