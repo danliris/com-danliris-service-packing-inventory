a007373f7a388afacbbd29250a951a334fde0f34
ardana96 <ardanagama@gmail.com>
2023-08-02 10:17:48 +0700

    Inv Transaction - Shipping Retur Receipt

---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingWarehouse/ShippingRetur/IDPShippingReturService.cs
@@ -0,0 +1,19 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.DyeingPrintingWarehouse.ShippingIN.ViewModel;
+using Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.DyeingPrintingWarehouse.ShippingRetur.ViewModel;
+using Com.Danliris.Service.Packing.Inventory.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.DyeingPrintingWarehouse.ShippingRetur
+{
+    public interface IDPShippingReturService
+    {
+        ListResult<IndexViewModel> Read(int page, int size, string filter, string order, string keyword);
+        Task<int> Create(ShippingReturViewModel viewModel);
+        ListResult<barcodeViewModel> GetCodeLoader(int page, int size, string filter, string order, string keyword);
+        Task<ShippingReturViewModel> ReadById(int id);
+        //int CheckBarcode(string barcode);
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/DyeingPrintingWarehouse/ShippingRetur/ViewModel/barcodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Com.Danliris.Service.Packing.Inventory.Application.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.DyeingPrintingWarehouse.ShippingRetur.ViewModel
+{
+    public class barcodeViewModel :BaseViewModel
+    {
+        public string Code { get;  set; }
+        public int FabricProductSKUId { get;  set; }
+        public int ProductSKUId { get;  set; }
+        public int ProductPackingId { get;  set; }
+        public int FabricPackingId { get; set; }
+    }
+}
