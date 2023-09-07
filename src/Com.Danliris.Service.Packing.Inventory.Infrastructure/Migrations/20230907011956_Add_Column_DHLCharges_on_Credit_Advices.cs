7ae5632b2238ca8a9fe98ea1d7f8c138c2f314ad
AndreaZain <andrea.kinan@gmail.com>
2023-09-07 08:29:00 +0700

    Enhance Credit Advices
    Add DHL Charges & Enhance Formula Nett Nego

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20230907011956_Add_Column_DHLCharges_on_Credit_Advices.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class Add_Column_DHLCharges_on_Credit_Advices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DHLCharges",
+                table: "GarmentShippingCreditAdvices",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LessFabricCost",
+                table: "GarmentShippingCreditAdvices",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DHLCharges",
+                table: "GarmentShippingCreditAdvices");
+
+            migrationBuilder.DropColumn(
+                name: "LessFabricCost",
+                table: "GarmentShippingCreditAdvices");
+        }
+    }
+}
