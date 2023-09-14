427fe9c78ddabaee94b7e30884232fc83e18f6b6
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-09-14 10:50:18 +0700

    enhance-SubconPL-addTotalQtyPackOut-for-Validation
    -change function CRUD in PackingListSubcon

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20230914021435_add-Column-TotalQtyPackOut-GReceiptSubconPL.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class addColumnTotalQtyPackOutGReceiptSubconPL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalQuantityPackingOut",
+                table: "GarmentReceiptSubconPackingListItems",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalQuantityPackingOut",
+                table: "GarmentReceiptSubconPackingListItems");
+        }
+    }
+}
