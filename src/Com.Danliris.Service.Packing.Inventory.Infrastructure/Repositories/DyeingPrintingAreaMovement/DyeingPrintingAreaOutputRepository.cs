64975caf2d3a59d30b29488d9a95f5cd20e9c0cd
ardana96 <ardanagama@gmail.com>
2024-09-25 08:57:46 +0700

    Bugfix Shipping - SPRINT 20 - 01

--- src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Repositories/DyeingPrintingAreaMovement/DyeingPrintingAreaOutputRepository.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Repositories/DyeingPrintingAreaMovement/DyeingPrintingAreaOutputRepository.cs
@@ -144,9 +144,9 @@ namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.Dye
                 }
 
             }
-
-            //model.FlagForDelete(_identityProvider.Username, UserAgent);
-            _dbSet.Update(model);
+            
+            model.FlagForDelete(_identityProvider.Username, UserAgent);
+            //_dbSet.Update(model);
 
             result += await _dbContext.SaveChangesAsync();
             return result;
@@ -267,7 +267,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Repositories.Dye
         {
             return _dbSet.Include(s => s.DyeingPrintingAreaOutputProductionOrders).IgnoreQueryFilters().AsNoTracking();
         }
-
+        
         public Task<DyeingPrintingAreaOutputModel> ReadByIdAsync(int id)
         {
             return _dbSet.Include(s => s.DyeingPrintingAreaOutputProductionOrders).FirstOrDefaultAsync(s => s.Id == id);
