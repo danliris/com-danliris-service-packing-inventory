3759e8055ed8fb226f1434545f21ac13295bc2a7
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-10-18 09:48:03 +0700

    2024/22/Enhance/PackingLIst-Update-maxLength-FinalDestinasion
    - from 50 to 500

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20241018024424_Update-Coloum-PackingList-FinalDestination-MaxLenght.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class UpdateColoumPackingListFinalDestinationMaxLenght : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FinalDestination",
+                table: "GarmentPackingLists",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+         
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+         
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FinalDestination",
+                table: "GarmentPackingLists",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+
+           
+        }
+    }
+}
