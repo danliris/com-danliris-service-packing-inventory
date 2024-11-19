9cb9f28a13a9e10c32b52dd442ad0a02f39e2d5c
yokana888 <yokana.adita@outlook.com>
2024-11-19 08:12:29 +0700

    AddMenuGarmentLocalSalesContract

---
+++ src/Com.Danliris.Service.Packing.Inventory.Data/Models/GarmentShipping/LocalSalesContract/GarmentMDLocalSalesContractModel.cs
@@ -0,0 +1,47 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Packing.Inventory.Data.Models.GarmentShipping.LocalSalesContract
+{
+    public class GarmentMDLocalSalesContractModel : StandardEntity
+    {
+        public string SalesContractNo { get; private set; }
+        public DateTimeOffset SalesContractDate { get; private set; }
+
+        public int TransactionTypeId { get; set; }
+        public string TransactionTypeCode { get; set; }
+        public string TransactionTypeName { get; set; }
+
+        public string SellerName { get; private set; }
+        public string SellerPosition { get; private set; }
+        public string SellerAddress { get; private set; }
+        public string SellerNPWP { get; private set; }
+
+        public int BuyerId { get; private set; }
+        public string BuyerCode { get; private set; }
+        public string BuyerName { get; private set; }
+        public string BuyerAddress { get; private set; }
+        public string BuyerNPWP { get; private set; }
+
+        public bool IsUseVat { get; private set; }
+        public int VatId { get; private set; }
+        public int VatRate { get; private set; }
+
+        public string ComodityName { get; private set; }
+
+        public double Quantity { get; private set; }
+        public double RemainingQuantity { get; private set; }
+
+        public int UomId { get; private set; }
+        public string UomUnit { get; private set; }
+        public double Price { get; private set; }
+
+        public string Remark { get; private set; }
+
+        public decimal SubTotal { get; private set; }
+
+        public bool IsLocalSalesDOCreated { get; private set; }
+    }
+}
---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20241118095944_Add-Table-For-NewLocalSalesContract.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class AddTableForNewLocalSalesContract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GarmentMDLocalSalesContracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Active = table.Column<bool>(nullable: false),
+                    CreatedUtc = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    CreatedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedUtc = table.Column<DateTime>(nullable: false),
+                    LastModifiedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    LastModifiedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedUtc = table.Column<DateTime>(nullable: false),
+                    DeletedBy = table.Column<string>(maxLength: 128, nullable: true),
+                    DeletedAgent = table.Column<string>(maxLength: 128, nullable: true),
+                    SalesContractNo = table.Column<string>(maxLength: 50, nullable: true),
+                    SalesContractDate = table.Column<DateTimeOffset>(nullable: false),
+                    TransactionTypeId = table.Column<int>(nullable: false),
+                    TransactionTypeCode = table.Column<string>(maxLength: 100, nullable: true),
+                    TransactionTypeName = table.Column<string>(maxLength: 250, nullable: true),
+                    SellerName = table.Column<string>(maxLength: 100, nullable: true),
+                    SellerPosition = table.Column<string>(maxLength: 100, nullable: true),
+                    SellerAddress = table.Column<string>(maxLength: 4000, nullable: true),
+                    SellerNPWP = table.Column<string>(maxLength: 50, nullable: true),
+                    BuyerId = table.Column<int>(nullable: false),
+                    BuyerCode = table.Column<string>(maxLength: 100, nullable: true),
+                    BuyerName = table.Column<string>(maxLength: 250, nullable: true),
+                    BuyerAddress = table.Column<string>(maxLength: 4000, nullable: true),
+                    BuyerNPWP = table.Column<string>(maxLength: 50, nullable: true),
+                    IsUseVat = table.Column<bool>(nullable: false),
+                    VatId = table.Column<int>(nullable: false),
+                    VatRate = table.Column<int>(nullable: false),
+                    ComodityName = table.Column<string>(maxLength: 250, nullable: true),
+                    Quantity = table.Column<double>(nullable: false),
+                    RemainingQuantity = table.Column<double>(nullable: false),
+                    UomId = table.Column<int>(nullable: false),
+                    UomUnit = table.Column<string>(maxLength: 250, nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    Remark = table.Column<string>(nullable: true),
+                    SubTotal = table.Column<decimal>(nullable: false),
+                    IsLocalSalesDOCreated = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GarmentMDLocalSalesContracts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GarmentMDLocalSalesContracts_SalesContractNo",
+                table: "GarmentMDLocalSalesContracts",
+                column: "SalesContractNo",
+                unique: true,
+                filter: "[IsDeleted]=(0)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GarmentMDLocalSalesContracts");
+        }
+    }
+}
