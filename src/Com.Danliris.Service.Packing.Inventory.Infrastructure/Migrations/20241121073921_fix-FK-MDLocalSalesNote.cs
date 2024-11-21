813c6ff6a834e8a909119d187005748e0985462c
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-11-21 16:46:21 +0700

    2024/24/Enhance/Add-Menu-BC25

---
+++ src/Com.Danliris.Service.Packing.Inventory.Infrastructure/Migrations/20241121073921_fix-FK-MDLocalSalesNote.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Packing.Inventory.Infrastructure.Migrations
+{
+    public partial class fixFKMDLocalSalesNote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItems_GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails");
+
+            migrationBuilder.DropColumn(
+                name: "GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GarmentMDLocalSalesNoteDetails_LocalSalesNoteItemId",
+                table: "GarmentMDLocalSalesNoteDetails",
+                column: "LocalSalesNoteItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItems_LocalSalesNoteItemId",
+                table: "GarmentMDLocalSalesNoteDetails",
+                column: "LocalSalesNoteItemId",
+                principalTable: "GarmentMDLocalSalesNoteItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItems_LocalSalesNoteItemId",
+                table: "GarmentMDLocalSalesNoteDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GarmentMDLocalSalesNoteDetails_LocalSalesNoteItemId",
+                table: "GarmentMDLocalSalesNoteDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails",
+                column: "GarmentMDLocalSalesNoteItemModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GarmentMDLocalSalesNoteDetails_GarmentMDLocalSalesNoteItems_GarmentMDLocalSalesNoteItemModelId",
+                table: "GarmentMDLocalSalesNoteDetails",
+                column: "GarmentMDLocalSalesNoteItemModelId",
+                principalTable: "GarmentMDLocalSalesNoteItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
