65888752bc8fa68dd1ae1411d5158f8f357ce9ed
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-12-03 13:59:43 +0700

    2024/25/Fix/PackingList-by-OrderNo
    - fix pdf

--- src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListWithHeaderPdfByOrderNoTemplate.cs
+++ src/Com.Danliris.Service.Packing.Inventory.Application/ToBeRefactored/GarmentShipping/GarmentPackingList/GarmentPackingListWithHeaderPdfByOrderNoTemplate.cs
@@ -120,7 +120,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
                 {
                     foreach (var size in detail.Sizes)
                     {
-                        sizesMax[size.Size.SizeIdx] = size.Size.Size;
+                        sizesMax[size.Size.Id] = size.Size.Size;
                     }
                 }
                 if (sizesMax.Count > 11)
@@ -232,7 +232,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
                 {
                     foreach (var size in detail.Sizes)
                     {
-                        sizes[size.Size.SizeIdx] = size.Size.Size;
+                        sizes[size.Size.Id] = size.Size.Size;
                     }
                 }
 
@@ -319,7 +319,7 @@ namespace Com.Danliris.Service.Packing.Inventory.Application.ToBeRefactored.Garm
                         double quantity = 0;
                         if (size.Key != 0)
                         {
-                            quantity = detail.Sizes.Where(w => w.Size.SizeIdx == size.Key).Sum(s => s.Quantity);
+                            quantity = detail.Sizes.Where(w => w.Size.Id == size.Key).Sum(s => s.Quantity);
                         }
 
                         if (sizeSumQty.ContainsKey(size.Key))
